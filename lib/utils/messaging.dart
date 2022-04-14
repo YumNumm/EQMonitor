8b8ec48953ff8a3b687fb664298432a5cc72677e
YumNumm <onory0917@icloud.com>
2022-04-15 01:40:16 +0900

    通知条件指定で起きていたバグの修正および、軽微な修正

--- lib/utils/messaging.dart
+++ lib/utils/messaging.dart
@@ -353,6 +353,8 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   //? 通知条件をクリアしているかをチェックする
   final j =
       json.decode(message.data['content'].toString()) as Map<String, dynamic>;
+  var shouldNotif = false;
+
   if (j['channelKey'].toString() == 'eew_forecast') {
     final Map<String, dynamic> fssData = await fss.readAll();
     final state = NotificationSettingsState(
@@ -363,7 +365,6 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
       notifOnUpwardUpdate:
           fssData['notifOnUpwardUpdate'].toString().parseBool(),
     );
-    var shouldNotif = false;
     if (state.notifAll) shouldNotif = true;
     try {
       final condition =
@@ -381,39 +382,40 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
       if (state.notifOnUpwardUpdate && payload[3]) {
         shouldNotif = true;
       }
-      if (shouldNotif) {
-        await AwesomeNotifications()
-            .createNotificationFromJsonData(message.data);
-        final flutterTts = FlutterTts();
-        await flutterTts.setLanguage('ja-JP');
-        if (message.data['tts'] != null) {
-          await flutterTts.speak(message.data['tts'].toString());
-        }
-        if (bool.fromEnvironment(
-          fss.read(key: 'toTweet').toString(),
-          defaultValue: true,
-        )) {
-          final AT = await fss.read(key: 'AT');
-          final AS = await fss.read(key: 'AS');
-          if (AT != null && AS != null) {
-            final twitterApi = TwitterApi(
-              client: TwitterClient(
-                consumerKey: clientCredentials.token,
-                consumerSecret: clientCredentials.tokenSecret,
-                token: AT,
-                secret: AS,
-              ),
-            );
-            //print(AT + AS);
-            final res = await twitterApi.tweetService.update(
-              status:
-                  '${message.data['content']['title']}\n${message.data['content']['body']}',
-            );
-          }
-        }
-      }
     } catch (e) {
       print(e);
+    }
+  } else {
+    shouldNotif = true;
+  }
+  if (shouldNotif) {
+    await AwesomeNotifications().createNotificationFromJsonData(message.data);
+    final flutterTts = FlutterTts();
+    await flutterTts.setLanguage('ja-JP');
+    if (message.data['tts'] != null) {
+      await flutterTts.speak(message.data['tts'].toString());
+    }
+    if (bool.fromEnvironment(
+      fss.read(key: 'toTweet').toString(),
+      defaultValue: true,
+    )) {
+      final AT = await fss.read(key: 'AT');
+      final AS = await fss.read(key: 'AS');
+      if (AT != null && AS != null) {
+        final twitterApi = TwitterApi(
+          client: TwitterClient(
+            consumerKey: clientCredentials.token,
+            consumerSecret: clientCredentials.tokenSecret,
+            token: AT,
+            secret: AS,
+          ),
+        );
+        //print(AT + AS);
+        final res = await twitterApi.tweetService.update(
+          status:
+              '${message.data['content']['title']}\n${message.data['content']['body']}',
+        );
+      }
     }
   }
 }
