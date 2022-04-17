8db377d8abeeacb07ba7e127596d5eedea1dbd08
YumNumm <onory0917@icloud.com>
2022-04-18 01:58:33 +0900

    Twitter連携前に警告文を表示

--- lib/pages/settings/accountSettingPage.dart
+++ lib/pages/settings/accountSettingPage.dart
@@ -32,109 +32,159 @@ Widget accountSettingPage() {
                   const Text('Twitterでログインすることにより、すべての地震に関する通知がツイートされます'),
               leading: const Icon(Icons.people),
               onPressed: (e) async {
-                _isProcessing = true;
-                final platform = oauth1.Platform(
-                  'https://api.twitter.com/oauth/request_token',
-                  'https://api.twitter.com/oauth/authorize',
-                  'https://api.twitter.com/oauth/access_token',
-                  oauth1.SignatureMethods.hmacSha1,
-                );
-
-                final auth = oauth1.Authorization(clientCredentials, platform);
-                oauth1.Credentials? tokenCredentials;
-                final res = await auth.requestTemporaryCredentials('oob');
-                tokenCredentials = res.credentials;
-                // launch() で ログイン用URLを開く
-                await launch(
-                  auth.getResourceOwnerAuthorizationURI(tokenCredentials.token),
-                );
-
-                final pinCodeController = TextEditingController();
-                await Get.dialog<void>(
-                  AlertDialog(
-                    title: const Text('PINコードを入力'),
-                    content: TextFormField(
-                      textInputAction: TextInputAction.done,
-                      controller: pinCodeController,
-                      decoration: const InputDecoration(
-                        hintText: 'PINコード',
+                final isAllowed = await Get.dialog<bool>(
+                      AlertDialog(
+                        title: const Text(
+                          '本当に続行しますか?',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        backgroundColor: const Color.fromARGB(255, 142, 0, 0),
+                        content: const Text(
+                          'アプリで受け取ったすべての通知を自動的にツイートします。\n'
+                          'そのため、フォロワーさんに迷惑をかけてしまう場合があります。\n'
+                          '十分注意してご利用ください。',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        scrollable: true,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () {
+                              Get.back<bool>(result: false);
+                            },
+                            child: const Text(
+                              'キャンセル',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Get.back<bool>(result: true);
+                            },
+                            child: const Text(
+                              '続行',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          )
+                        ],
                       ),
-                      validator: (e) {
-                        if (int.tryParse(e.toString()) == null) {
-                          return '正しいPINコードを入力してください';
-                        }
-                        return null;
-                      },
-                      autofocus: true,
-                      keyboardType: TextInputType.number,
-                      autovalidateMode: AutovalidateMode.always,
+                    ) ??
+                    true;
+                if (isAllowed) {
+                  _isProcessing = true;
+                  final platform = oauth1.Platform(
+                    'https://api.twitter.com/oauth/request_token',
+                    'https://api.twitter.com/oauth/authorize',
+                    'https://api.twitter.com/oauth/access_token',
+                    oauth1.SignatureMethods.hmacSha1,
+                  );
+
+                  final auth =
+                      oauth1.Authorization(clientCredentials, platform);
+                  oauth1.Credentials? tokenCredentials;
+                  final res = await auth.requestTemporaryCredentials('oob');
+                  tokenCredentials = res.credentials;
+                  // launch() で ログイン用URLを開く
+                  await launch(
+                    auth.getResourceOwnerAuthorizationURI(
+                      tokenCredentials.token,
                     ),
-                    actions: [
-                      TextButton(
-                        onPressed: () async {
-                          pinCodeController.text = 'canceled';
-                          Get.back<void>();
-                        },
-                        child: const Text('キャンセル'),
-                      ),
-                      TextButton(
-                        onPressed: () async {
-                          logger.d(pinCodeController.text);
-                          if (int.tryParse(pinCodeController.text) != null) {
-                            Get.snackbar('ログイン中...', 'しばらくお待ちください。');
-                            await Get.showOverlay(
-                              loadingWidget: const Center(
-                                child: CircularProgressIndicator(),
-                              ),
-                              asyncFunction: () async {
-                                //OK Next Step
-                                final res = await auth.requestTokenCredentials(
-                                  tokenCredentials!,
-                                  pinCodeController.text,
-                                );
-                                if (res.credentials.token == null) {
-                                  //ERROR
-                                  Get.back<void>();
-                                  return;
-                                }
-                                await fss.write(
-                                  key: 'AT',
-                                  value: res.credentials.token,
-                                );
-                                await fss.write(
-                                  key: 'AS',
-                                  value: res.credentials.tokenSecret,
-                                );
-                                final credential =
-                                    TwitterAuthProvider.credential(
-                                  accessToken: res.credentials.token,
-                                  secret: res.credentials.tokenSecret,
-                                );
-                                await FirebaseAuth.instance
-                                    .signInWithCredential(credential);
-                                await authStateUtils.onInit();
-                              },
-                            );
-                            Get.closeAllSnackbars();
-                            Get.snackbar(
-                              'ログイン成功',
-                              'ようこそ ${authStateUtils.user.value!.displayName}さん!',
-                            );
-                            Get.back<void>();
-                          } else {
-                            Get.snackbar(
-                              'PINコードを入力してください',
-                              '',
-                              backgroundColor: Colors.redAccent,
-                            );
+                  );
+
+                  final pinCodeController = TextEditingController();
+                  await Get.dialog<void>(
+                    AlertDialog(
+                      title: const Text('PINコードを入力'),
+                      content: TextFormField(
+                        textInputAction: TextInputAction.done,
+                        controller: pinCodeController,
+                        decoration: const InputDecoration(
+                          hintText: 'PINコード',
+                        ),
+                        validator: (e) {
+                          if (int.tryParse(e.toString()) == null) {
+                            return '正しいPINコードを入力してください';
                           }
-                          Get.back<void>();
+                          return null;
                         },
-                        child: const Text('次に進む'),
+                        autofocus: true,
+                        keyboardType: TextInputType.number,
+                        autovalidateMode: AutovalidateMode.always,
                       ),
-                    ],
-                  ),
-                );
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () async {
+                            Get.back<void>();
+                          },
+                          child: const Text('キャンセル'),
+                        ),
+                        TextButton(
+                          onPressed: () async {
+                            logger.d(pinCodeController.text);
+                            if (int.tryParse(pinCodeController.text) != null) {
+                              Get.snackbar('ログイン中...', 'しばらくお待ちください。');
+                              await Get.showOverlay(
+                                loadingWidget: const Center(
+                                  child: CircularProgressIndicator(),
+                                ),
+                                asyncFunction: () async {
+                                  //OK Next Step
+                                  final res =
+                                      await auth.requestTokenCredentials(
+                                    tokenCredentials!,
+                                    pinCodeController.text,
+                                  );
+                                  if (res.credentials.token == null) {
+                                    //ERROR
+                                    Get.back<void>();
+                                    return;
+                                  }
+                                  await fss.write(
+                                    key: 'AT',
+                                    value: res.credentials.token,
+                                  );
+                                  await fss.write(
+                                    key: 'AS',
+                                    value: res.credentials.tokenSecret,
+                                  );
+                                  final credential =
+                                      TwitterAuthProvider.credential(
+                                    accessToken: res.credentials.token,
+                                    secret: res.credentials.tokenSecret,
+                                  );
+                                  await FirebaseAuth.instance
+                                      .signInWithCredential(credential);
+                                  await authStateUtils.onInit();
+                                },
+                              );
+                              Get.closeAllSnackbars();
+                              Get.snackbar(
+                                'ログイン成功',
+                                'ようこそ ${authStateUtils.user.value!.displayName}さん!',
+                              );
+                            } else {
+                              Get.snackbar(
+                                'PINコードを入力してください',
+                                '',
+                                backgroundColor: Colors.redAccent,
+                              );
+                            }
+                            Get.back<void>();
+                          },
+                          child: const Text('次に進む'),
+                        ),
+                      ],
+                    ),
+                  );
+                }
               },
             ),
           ],
