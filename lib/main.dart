6bc55cba322ed4b85d396c551d8f026951f9758f
YumNumm <onory0917@icloud.com>
2022-04-21 01:49:20 +0900

    some

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_performance/firebase_performance.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -47,9 +48,9 @@ Future<void> main() async {
   final prefs =
       Get.put<SharedPreferences>(await SharedPreferences.getInstance());
   await Firebase.initializeApp();
-  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(!kDebugMode);
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
-  
+
   Get.put<Logger>(
     Logger(
       level: Level.debug,
