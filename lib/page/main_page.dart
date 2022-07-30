4c15fd2231e1aa2cb852a1eeea53bb1621271465
YumNumm <onory0917@icloud.com>
2022-07-30 21:03:12 +0900

    ページ切り替え時にHapticFeedbackをするように

--- lib/page/main_page.dart
+++ lib/page/main_page.dart
@@ -2,6 +2,7 @@ import 'package:eqmonitor/page/main/earthquake_history.dart';
 import 'package:eqmonitor/page/main/kmoni_map.dart';
 import 'package:eqmonitor/state/all_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -37,7 +38,11 @@ class _MainPageState extends ConsumerState<MainPage> {
       ),
       bottomNavigationBar: NavigationBar(
         selectedIndex: selectedIndex.value,
-        onDestinationSelected: (i) => selectedIndex.value = i,
+        onDestinationSelected: (i) {
+          HapticFeedback.selectionClick();
+
+          selectedIndex.value = i;
+        },
         destinations: const [
           NavigationDestination(icon: Icon(Icons.home), label: '強震モニタ'),
           NavigationDestination(icon: Icon(Icons.history), label: '地震履歴'),
