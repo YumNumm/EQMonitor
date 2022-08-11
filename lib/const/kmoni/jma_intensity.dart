19c59c241a37973aecfcba7b58414cba7e57762c
YumNumm <73390859+YumNumm@users.noreply.github.com>
2022-08-11 15:48:00 +0900

    Update jma_intensity.dart

--- lib/const/kmoni/jma_intensity.dart
+++ lib/const/kmoni/jma_intensity.dart
@@ -18,31 +18,31 @@ enum JmaIntensity {
   Int0('0', Color.fromARGB(255, 242, 242, 255), true, 0),
 
   /// 震度1
-  Int1('1', Color.fromARGB(255, 242, 242, 255), true, 1),
+  Int1('1', Color.fromARGB(255, 107, 120, 120), true, 1),
 
   /// 震度2
-  Int2('2', Color.fromARGB(255, 0, 170, 255), false, 2),
+  Int2('2', Color.fromARGB(255, 30, 110, 230), false, 2),
 
   /// 震度3
-  Int3('3', Color.fromARGB(255, 45, 244, 255), true, 3),
+  Int3('3', Color.fromARGB(255, 50, 180, 100), true, 3),
 
   /// 震度4
-  Int4('4', Color.fromARGB(255, 255, 213, 0), true, 4),
+  Int4('4', Color.fromARGB(255, 255, 224, 93), true, 4),
 
   /// 震度5弱
-  Int5Lower('5-', Color.fromARGB(255, 255, 153, 0), true, 5),
+  Int5Lower('5-', Color.fromARGB(255, 255, 170, 19), true, 5),
 
   /// 震度5強
-  Int5Upper('5+', Color.fromARGB(255, 255, 77, 0), true, 6),
+  Int5Upper('5+', Color.fromARGB(255, 255, 112, 15), true, 6),
 
   /// 震度6弱
-  Int6Lower('6-', Color.fromARGB(255, 255, 57, 57), false, 7),
+  Int6Lower('6-', Color.fromARGB(255, 230, 0, 0), false, 7),
 
   /// 震度6強
-  Int6Upper('6+', Color.fromARGB(255, 254, 25, 162), false, 8),
+  Int6Upper('6+', Color.fromARGB(255, 160, 0, 0), false, 8),
 
   /// 震度7
-  Int7('7', Color.fromARGB(255, 188, 0, 226), false, 9),
+  Int7('7', Color.fromARGB(255, 93, 0, 144), false, 9),
 
   // 程度以上
   over('over', Color.fromARGB(255, 255, 255, 255), true, -1),
