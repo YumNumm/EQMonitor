ac90a6dd30c4a8df50b7daacfa82b0a4879d2491
YumNumm <onory0917@icloud.com>
2022-08-19 15:29:01 +0900

    add. actions:地盤増幅率ファイル生成

---
+++ util/arv/main.py
@@ -0,0 +1,64 @@
+import requests
+import json
+import os
+import datetime
+
+
+DMDATA_KEY = os.getenv("DMDATA_API_KEY")
+DMDATA_ORIGIN = os.getenv("DMDATA_ORIGIN")
+
+def downloadParam():
+    url = f"https://api.dmdata.jp/v2/parameter/earthquake/station?key={DMDATA_KEY}"
+    res = requests.get(
+        url,
+        headers={"Origin": DMDATA_ORIGIN},
+    )
+    with open("station.json", "wb") as file:
+        for chunk in res.iter_content(chunk_size=1024):
+            file.write(chunk)
+
+
+def getArv(lat, lon):
+    res = requests.get(
+        f"https://www.j-shis.bosai.go.jp/map/api/sstrct/V2/meshinfo.geojson?position={lon},{lat}&epsg=4612"
+    )
+    j = res.json()
+    arv = j["features"][0]["properties"]["ARV"]
+    return arv
+
+
+downloadParam()
+
+with open(
+    "./station.json",
+    encoding="utf-8",
+) as f:
+    os.makedirs("./public", exist_ok=True)
+    with open(
+        "./public/arv.json",
+        "w",
+        newline="",
+        encoding="utf-8",
+    ) as w:
+        j = json.loads(f.read())
+        items = []
+        for i in j["items"]:
+            arv = getArv(i["latitude"], i["longitude"])
+            print(arv)
+            items.append(
+                {
+                    "code": i["code"],
+                    "arv": arv,
+                }
+            )
+        dt = datetime.datetime.today()
+        output = {
+            "responseId": j["responseId"],
+            "responseTime": j["responseTime"],
+            "mergedAt": dt.isoformat(),
+            "status": j["status"],
+            "changeTime": j["changeTime"],
+            "version": j["version"],
+            "items": items,
+        }
+        json.dump(output, w)
