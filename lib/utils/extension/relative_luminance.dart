33773f2ef955f55b80717056cb5e6ec1ef7ba5d9
YumNumm <onory0917@gmail.com>
2022-10-24 20:04:02 +0900

    fix: 震度配色の背景・文字色の変更アルゴリズムを変更

--- lib/utils/extension/relative_luminance.dart
+++ lib/utils/extension/relative_luminance.dart
@@ -1,14 +1,23 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 /// 背景色が明るい色であれば、黒を、暗い色であれば白を返す
-/// ref: https://en.wikipedia.org/wiki/Relative_luminance
+/// ref: https://zenn.dev/mryhryki/articles/2020-11-12-hatena-background-color
 extension OnPrimary on Color {
-  /// 輝度が高ければ黒, 低ければ白を返す
   Color get onPrimary {
-    // 輝度により黒か白かを決定する
-    if (computeLuminance() < 100 / 256) {
-      return Colors.white;
-    }
-    return Colors.black;
+    final darkRatio = getContrastRatio(this, Colors.black);
+    final lightRatio = getContrastRatio(this, Colors.white);
+
+    return darkRatio > lightRatio ? Colors.black : Colors.white;
   }
+}
+
+double getContrastRatio(Color color1, Color color2) {
+  final luminance1 = color1.computeLuminance();
+  final luminance2 = color2.computeLuminance();
+
+  final bright = max(luminance1, luminance2);
+  final dark = min(luminance1, luminance2);
+  return (bright + 0.05) / (dark + 0.05);
 }
