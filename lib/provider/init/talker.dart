466aae438e0b97365d6c55ae6c8fce39d0bde491
YumNumm <onory0917@gmail.com>
2022-11-20 21:58:40 +0900

    add: talker_flutterライブラリの追加

---
+++ lib/provider/init/talker.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:talker_flutter/talker_flutter.dart';
+
+final talkerProvider = Provider<Talker>((ref) => throw UnimplementedError());
