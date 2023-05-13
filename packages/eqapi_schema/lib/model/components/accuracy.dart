f897239dfe27f17bfb37f120d97349c389fae569
YumNumm <onory0917@gmail.com>
2023-05-13 21:14:41 +0900

    fix: [TelegramV3] typedef

--- packages/eqapi_schema/lib/model/components/accuracy.dart
+++ packages/eqapi_schema/lib/model/components/accuracy.dart
@@ -6,10 +6,10 @@ part 'accuracy.g.dart';
 @freezed
 class EewAccuracy with _$EewAccuracy {
   const factory EewAccuracy({
-    required List<int> epicenters,
-    required int depth,
-    required int magnitudeCalculation,
-    required int numberOfMagnitudeCalculation,
+    required List<String> epicenters,
+    required String depth,
+    required String magnitudeCalculation,
+    required String numberOfMagnitudeCalculation,
   }) = _EewAccuracy;
 
   factory EewAccuracy.fromJson(Map<String, dynamic> json) =>
