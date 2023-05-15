33760da58490ac3aca34051f2a916f2a61c4d734
YumNumm <onory0917@gmail.com>
2023-05-15 14:10:07 +0900

    add: customchip

---
+++ lib/common/component/chip/custom_chip.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomChip extends StatelessWidget {
+  const CustomChip({required this.child, super.key});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Chip(
+      label: child,
+      elevation: 0,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(8),
+        ),
+        side: BorderSide(
+          color: Colors.white,
+          width: 0,
+        ),
+      ),
+      side: const BorderSide(
+        color: Colors.white,
+        width: 0,
+      ),
+      padding: EdgeInsets.zero,
+    );
+  }
+}
