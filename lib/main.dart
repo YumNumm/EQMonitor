106edfe3bc8aa063fba3be9bbe97e2f6c7aab79b
YumNumm <onory0917@gmail.com>
2023-05-20 20:56:53 +0900

    add: firebase crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'dart:io';
 
 import 'package:awesome_notifications_fcm/awesome_notifications_fcm.dart';
 import 'package:eqmonitor/app.dart';
+import 'package:eqmonitor/common/provider/fcm_token.dart';
 import 'package:eqmonitor/common/provider/shared_preferences.dart';
 import 'package:eqmonitor/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -18,23 +20,37 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Pass all uncaught "fatal" errors from the framework to Crashlytics
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
   final prefs = await SharedPreferences.getInstance();
+  String? fcmToken;
   if (Platform.isAndroid || Platform.isIOS) {
-    final fcmToken = await getFirebaseMessagingToken();
-    log('Firebase token: $fcmToken');
-    if (kDebugMode) {
-      unawaited(
-        FirebaseMessaging.instance.subscribeToTopic('config-developer'),
-      );
-    }
-    unawaited(FirebaseMessaging.instance.subscribeToTopic('everyone'));
-    unawaited(FirebaseMessaging.instance.subscribeToTopic('eew'));
-    unawaited(FirebaseMessaging.instance.subscribeToTopic('earthquake'));
+    fcmToken = await getFirebaseMessagingToken();
+
+    unawaited(() async {
+      log('Firebase token: $fcmToken');
+      if (kDebugMode) {
+        unawaited(
+          FirebaseMessaging.instance.subscribeToTopic('config-developer'),
+        );
+      }
+      await FirebaseMessaging.instance.subscribeToTopic('everyone');
+      await FirebaseMessaging.instance.subscribeToTopic('eew');
+      await FirebaseMessaging.instance.subscribeToTopic('earthquake');
+    }());
   }
   return runApp(
     ProviderScope(
       overrides: [
         sharedPreferencesProvider.overrideWithValue(prefs),
+        if (fcmToken != null) fcmTokenProvider.overrideWithValue(fcmToken),
       ],
       child: const App(),
     ),
