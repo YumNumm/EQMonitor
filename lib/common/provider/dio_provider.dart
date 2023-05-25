9d024948007149b4773d617b7a9cd1efb4665f9b
YumNumm <onory0917@gmail.com>
2023-05-26 03:56:49 +0900

    fix: api周りの修正 - HTTP2対応 - Header追加

--- lib/common/provider/dio_provider.dart
+++ lib/common/provider/dio_provider.dart
@@ -1,9 +1,27 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
+import 'package:dio_http2_adapter/dio_http2_adapter.dart';
+import 'package:flutter/foundation.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'dio_provider.g.dart';
 
 @Riverpod(keepAlive: true)
 Dio dio(DioRef ref) {
-  return Dio();
+  return Dio(
+    BaseOptions(
+      headers: {
+        'user-agent': 'eqmonitor-${Platform.version}',
+        'x-operation-system-version': Platform.operatingSystemVersion,
+      },
+    ),
+  )..httpClientAdapter = Http2Adapter(
+      ConnectionManager(
+        idleTimeout: const Duration(seconds: 10),
+        // debug時は証明書の検証を行わない
+        onClientCreate: (_, config) =>
+            config.onBadCertificate = (_) => kDebugMode,
+      ),
+    );
 }
