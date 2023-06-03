02a6265519342231d8921e5cde48d997db8bc5b3
YumNumm <onory0917@gmail.com>
2023-06-04 00:31:50 +0900

    fix: eewTelegramProviderの型定義

--- lib/feature/home/providers/telegram_ws/provider/eew_telegram_provider.dart
+++ lib/feature/home/providers/telegram_ws/provider/eew_telegram_provider.dart
@@ -1,23 +1,28 @@
 import 'dart:async';
 
 import 'package:eqapi_schema/eqapi_schema.dart';
+import 'package:eqmonitor/feature/home/providers/telegram_ws/model/eew_model.dart';
 import 'package:eqmonitor/feature/home/providers/telegram_ws/provider/telegram_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'eew_telegram_provider.g.dart';
 
-@riverpod
+@Riverpod()
 class EewWsTelegram extends _$EewWsTelegram {
   @override
-  Stream<Vxse45> build() {
-    final controller = StreamController<Vxse45>();
+  Stream<EewWsItem> build() {
+    final controller = StreamController<EewWsItem>();
     ref
       ..listen(telegramWsProvider, (previous, next) {
         final value = next.value;
         if (value != null) {
           if (value.type == TelegramType.vxse45) {
-            // final data = (value, value.body as Vxse45);
-            // controller.add(data);
+            controller.add(
+              EewWsItem(
+                telegram: value,
+                body: value.body as Vxse45,
+              ),
+            );
           }
         }
       })
