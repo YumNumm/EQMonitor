be859cf13207aac070ffb04c5566845cbcb8c31d
YumNumm <onory0917@gmail.com>
2023-06-15 03:06:56 +0900

    fix: eew widget

--- lib/common/component/sheet/basic_modal_sheet.dart
+++ lib/common/component/sheet/basic_modal_sheet.dart
@@ -14,7 +14,6 @@ class BasicModalSheet extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-
     final barWidget = Container(
       margin: const EdgeInsets.symmetric(vertical: 8),
       width: 36,
@@ -28,37 +27,57 @@ class BasicModalSheet extends HookWidget {
         ],
       ),
     );
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        print(constraints);
+        final isTablet = constraints.maxWidth > 600 &&
+            constraints.maxHeight < constraints.maxWidth;
 
-    return Sheet(
-      backgroundColor: Colors.transparent,
-      initialExtent: 120,
-      controller: controller,
-      physics: const SnapSheetPhysics(
-        stops: <double>[0.1, 0.3, 0.5, 0.95, 1],
-      ),
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(16),
-            topRight: Radius.circular(16),
+        final sheet = Sheet(
+          backgroundColor: Colors.transparent,
+          initialExtent: 120,
+          controller: controller,
+          physics: const SnapSheetPhysics(
+            stops: <double>[0.1, 0.3, 0.5, 0.95, 1],
           ),
-          color: theme.colorScheme.surface,
-          boxShadow: const <BoxShadow>[
-            BoxShadow(color: Colors.black12, blurRadius: 12),
-          ],
-        ),
-        child: Column(
-          children: <Widget>[
-            barWidget,
-            Expanded(
-              child: ListView(
-                padding: const EdgeInsets.all(4),
-                children: children,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(16),
+                topRight: Radius.circular(16),
+              ),
+              color: theme.colorScheme.surface,
+              boxShadow: const <BoxShadow>[
+                BoxShadow(color: Colors.black12, blurRadius: 12),
+              ],
+            ),
+            child: Column(
+              children: <Widget>[
+                barWidget,
+                Expanded(
+                  child: ListView(
+                    padding: const EdgeInsets.all(4),
+                    children: children,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+        if (isTablet) {
+          return Align(
+            alignment: Alignment.bottomRight,
+            child: SafeArea(
+              bottom: false,
+              child: SizedBox(
+                width: constraints.maxWidth / 2,
+                child: sheet,
               ),
             ),
-          ],
-        ),
-      ),
+          );
+        }
+        return sheet;
+      },
     );
   }
 }
