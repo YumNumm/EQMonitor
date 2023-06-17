23732a3a983215f43325f29f49c4d6a80f99bc93
YumNumm <YumNumm@users.noreply.github.com>
2023-06-17 16:29:22 +0000

    Auto format

--- lib/feature/home/features/kmoni_observation_points/model/kmoni_observation_point.dart
+++ lib/feature/home/features/kmoni_observation_points/model/kmoni_observation_point.dart
@@ -44,10 +44,9 @@ class AnalyzedKmoniObservationPoint with _$AnalyzedKmoniObservationPoint {
     // ここから
     double? intensityValue,
     @JsonKey(fromJson: colorFromJson, toJson: colorToJson)
-        Color? intensityColor,
+    Color? intensityColor,
     double? pga,
-    @JsonKey(fromJson: colorFromJson, toJson: colorToJson)
-        Color? pgaColor,
+    @JsonKey(fromJson: colorFromJson, toJson: colorToJson) Color? pgaColor,
   }) = _AnalyzedKmoniObservationPoint;
 }
 
