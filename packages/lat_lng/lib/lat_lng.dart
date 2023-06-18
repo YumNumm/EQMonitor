a43a8e6bbb91be0341003da646a5cd4023198f40
YumNumm <onory0917@gmail.com>
2023-06-18 12:22:09 +0900

    add: latLng boundary

--- packages/lat_lng/lib/lat_lng.dart
+++ packages/lat_lng/lib/lat_lng.dart
@@ -1,8 +1,4 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library;
 
-export 'src/lat_lng_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/lat_lng.dart';
+export 'src/lat_lng_boundary.dart';
