96c99619f72fefae6353a99cc020863505db931f
YumNumm <onory0917@gmail.com>
2023-06-18 11:38:59 +0900

    lib: latLng

---
+++ packages/lat_lng/lib/src/lat_lng_base.dart
@@ -0,0 +1,24 @@
+import 'dart:math';
+
+class LatLng extends Point<double> {
+  const LatLng(this.lat, this.lon) : super(lat, lon);
+  factory LatLng.fromJson(Map<String, dynamic> json) {
+    return LatLng(
+      (json['lat'] as num).toDouble(),
+      (json['lon'] as num).toDouble(),
+    );
+  }
+
+  final double lat;
+  final double lon;
+
+  @override
+  String toString() {
+    return 'LatLng(lat: $lat, lon: $lon)';
+  }
+
+  Map<String, dynamic> toJson() => {
+        'lat': lat,
+        'lon': lon,
+      };
+}
