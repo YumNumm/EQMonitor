5d6c9592fb392fa18773d5c924648bb158fbbc1a
YumNumm <onory0917@gmail.com>
2023-07-17 03:23:31 +0900

    fix: introduction page

--- lib/feature/setup/pages/04_notification_setting.dart
+++ lib/feature/setup/pages/04_notification_setting.dart
@@ -1,4 +1,6 @@
 import 'package:eqmonitor/core/component/button/action_button.dart';
+import 'package:eqmonitor/core/component/container/bordered_container.dart';
+import 'package:eqmonitor/core/fcm/notification_controller.dart';
 import 'package:eqmonitor/feature/setup/component/background_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -15,54 +17,65 @@ class NotificationSettingPage extends HookWidget {
       body: SetupBackgroundImageWidget(
         child: Column(
           children: [
-            // アプリアイコン
-            const Padding(
-              padding: EdgeInsets.all(16),
-              child: SizedBox(
-                width: 100,
-                height: 100,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.all(Radius.circular(800)),
-                  child: Image(
-                    image: AssetImage('assets/images/icon.png'),
-                  ),
-                ),
-              ),
-            ),
             // 画面上部のタイトル
             Padding(
               padding: const EdgeInsets.all(16),
               child: Text(
-                'EQMonitorへようこそ',
+                '通知について',
                 style: theme.textTheme.titleLarge!.copyWith(
-                  fontWeight: FontWeight.bold,
+                  fontWeight: FontWeight.w900,
                   color: Colors.white,
                 ),
               ),
             ),
             const Spacer(),
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: ActionButton(
-                isEnabled: true,
-                onPressed: onNext,
-                child: const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 12),
-                  child: SizedBox(
-                    width: double.infinity,
-                    child: Center(
-                      child: Text(
-                        '始める',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
+            BorderedContainer(
+              child: Column(
+                children: [
+                  const SizedBox(height: 12),
+                  // notification icon
+                  const DecoratedBox(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(12),
+                      child: Icon(
+                        Icons.notifications_active,
+                        color: Colors.redAccent,
+                        size: 32,
                       ),
                     ),
                   ),
-                ),
+                  const SizedBox(height: 16),
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Text(
+                      '地震に関する情報をお伝えするために、通知権限を許可してください',
+                      style: theme.textTheme.titleMedium!.copyWith(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            )
+            ),
+            const Spacer(),
+            ActionButton.text(
+              onPressed: () => NotificationController.requestPermission().then(
+                (value) => onNext(),
+              ),
+              text: '通知を許可する',
+              context: context,
+            ),
+            const SizedBox(height: 16),
+            TextButton(
+              onPressed: onNext,
+              child: const Text('拒否する'),
+            ),
+            const Spacer(),
+            const Spacer(),
           ],
         ),
       ),
