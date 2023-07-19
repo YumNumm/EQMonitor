51bfabb41d10eb4ba217e9bc4cd044c31575da18
YumNumm <onory0917@gmail.com>
2023-07-19 18:09:01 +0900

    add: shader

--- shaders/introduction.glsl
+++ shaders/introduction.glsl
@@ -1,36 +1,19 @@
-#include <flutter/runtime_effect.glsl>
-
-// use nice time-based shader from http://editor.thebookofshaders.com/
-
+#include<flutter/runtime_effect.glsl>
 
 uniform float time;
 uniform vec2 resolution;
 
 out vec4 FragColor;
 
-float makePoint(float x,float y,float fx,float fy,float sx,float sy,float t){
-   float xx = x+sin(t*fx)*sx;
-   float yy = y+cos(t*fy)*sy;
-   return 3./sqrt(xx*xx+yy*yy);
-}
+void main(void){
+   vec2 p=(FlutterFragCoord().xy*2.-resolution)/min(resolution.x,resolution.y);
+   p+=vec2(cos(time/6.),sin(time/8.));
+   // 画面外に持っていく
+   p -= vec2(2.5,2.8);
+   float l=(1.+sin(time)/8.)/(length(p)+.5);
 
-void main( void ) {
+   float r = (1.+cos(time)/8.)/(length(p + vec2(cos(time/6.),sin(time/8.)) )+.5) / 10.;
+   float g = (1.+cos(time)/6.)/(length(p + vec2(cos(time/8.),sin(time/6.)) )+.5) / 10.;
 
-   vec2 p=(FlutterFragCoord().xy/resolution.x) - vec2(0.5,0.5);
-
-   float x=p.x;
-   float y=p.y;
-
-   float r=
-      0.06 / makePoint(x,y,6.,2.9,0.3,0.3,time);
-
-   float g=
-      0.06 / makePoint(x,y,1.2,1.9,0.3,0.3,time);
-
-   float b=
-      0.4 / makePoint(x,y,1.2,1.2,0.4,0.3,time);
-
-   vec3 d=vec3(r,g,b);
-
-   FragColor = vec4(d,1.0);
+   FragColor=vec4(r,g,l,1.);
 }
