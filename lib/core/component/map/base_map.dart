8d0f92b827d6e508c2d9a5d655b366c9cf74317b
YumNumm <onory0917@gmail.com>
2023-07-19 18:09:40 +0900

    add: baseMapにpolygonとpolylineオプション追加

--- lib/core/component/map/base_map.dart
+++ lib/core/component/map/base_map.dart
@@ -4,90 +4,90 @@ import 'package:eq_map/eq_map.dart';
 import 'package:eqmonitor/core/component/map/data/model/mutable_projected_feature_layer.dart';
 import 'package:eqmonitor/core/component/map/model/map_config.dart';
 import 'package:eqmonitor/core/component/map/model/map_state.dart';
-import 'package:eqmonitor/core/component/map/model/mutable/projected_feature_layer.dart';
-import 'package:eqmonitor/core/component/map/utils/web_mercator_projection.dart';
 import 'package:eqmonitor/core/component/map/view_model/map_viewmodel.dart';
 import 'package:eqmonitor/core/provider/topology_map/provider/topology_maps.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:topo_map/topo_map.dart';
 
 class BaseMapWidget extends HookConsumerWidget {
-  const BaseMapWidget({
-    super.key,
+  const BaseMapWidget._({
     required this.mapKey,
-    this.onlyBorder = false,
+    this.drawPolyline = true,
+    this.drawPolygon = true,
   });
+
+  factory BaseMapWidget.polygon(Key mapKey) => BaseMapWidget._(
+        mapKey: mapKey,
+        drawPolyline: false,
+      );
+
+  factory BaseMapWidget.polyline(Key mapKey) => BaseMapWidget._(
+        mapKey: mapKey,
+        drawPolygon: false,
+      );
+  factory BaseMapWidget.all(Key mapKey) => BaseMapWidget._(
+        mapKey: mapKey,
+      );
+
   final Key mapKey;
-  final bool onlyBorder;
+  final bool drawPolyline;
+  final bool drawPolygon;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final state = ref.watch(MapViewModelProvider(mapKey));
-    final map = ref.watch(mapDataProvider);
-    final mapData = map.valueOrNull;
-    if (mapData == null || mapData.maps == null) {
-      return const Center(
-        child: CircularProgressIndicator.adaptive(),
-      );
-    }
     final zoomCachedProjectedFeatureLayer =
-        useMemoized<Map<LandLayerType, ZoomCachedProjectedFeatureLayer>?>(
-      () => mapData.maps!.map(
-        (key, value) => MapEntry(
-          key,
-          ZoomCachedProjectedFeatureLayer.fromProjectedFeatureLayer(
-            ProjectedFeatureLayer.fromFeatureLayer(
-              layer: value,
-              projection: WebMercatorProjection(),
-            ),
-          ),
-        ),
-      ),
-      [mapData],
-    );
-    if (zoomCachedProjectedFeatureLayer == null) {
-      return const Center(
-        child: CircularProgressIndicator.adaptive(),
-      );
-    }
+        ref.watch(zoomCachedProjectedFeatureLayerProvider);
+
     final isDark = Theme.of(context).brightness == Brightness.dark;
 
-    return CustomPaint(
-      painter: _BaseMapPainter(
-        state: state,
-        onlyBorder: onlyBorder,
-        colorScheme: isDark ? MapColorScheme.dark() : MapColorScheme.light(),
-        // colorScheme: mapConfig.colorScheme,
-        maps: zoomCachedProjectedFeatureLayer,
-      ),
-      size: Size.infinite,
-    );
+    return switch (zoomCachedProjectedFeatureLayer.value) {
+      null => const Center(
+          child: CircularProgressIndicator.adaptive(),
+        ),
+      final data => CustomPaint(
+          painter: _BaseMapPainter(
+            state: ref.watch(MapViewModelProvider(mapKey)),
+            drawPolyline: drawPolyline,
+            drawPolygon: drawPolygon,
+            colorScheme:
+                isDark ? MapColorScheme.dark() : MapColorScheme.light(),
+            maps: data,
+          ),
+          size: Size.infinite,
+          willChange: true,
+        ),
+    };
   }
 }
 
 class _BaseMapPainter extends CustomPainter {
   _BaseMapPainter({
     required this.state,
-    required this.onlyBorder,
+    required this.drawPolyline,
+    required this.drawPolygon,
     required this.colorScheme,
     required this.maps,
   });
 
   final MapState state;
-  final bool onlyBorder;
   final MapColorScheme colorScheme;
   final Map<LandLayerType, ZoomCachedProjectedFeatureLayer> maps;
 
+  final bool drawPolyline;
+  final bool drawPolygon;
+
   @override
   void paint(Canvas canvas, Size size) {
-    baseColorPaint(canvas, colorScheme);
     // draw
-    drawJapanPolygon(canvas, size, colorScheme);
-    drawJapanPolyline(canvas, size, colorScheme);
-    drawWorldPolygon(canvas, size, colorScheme);
-    drawWorldPolyline(canvas, size, colorScheme);
+    if (drawPolyline) {
+      drawJapanPolyline(canvas, size, colorScheme);
+      drawWorldPolyline(canvas, size, colorScheme);
+    }
+    if (drawPolygon) {
+      drawJapanPolygon(canvas, size, colorScheme);
+      drawWorldPolygon(canvas, size, colorScheme);
+    }
     if (state.zoomLevel > 400) {
       //  drawJapanDetailedPolyline(canvas, size, colorScheme);
     }
@@ -284,14 +284,10 @@ class _BaseMapPainter extends CustomPainter {
     }
   }
 
-  void baseColorPaint(Canvas canvas, MapColorScheme colorScheme) {
-    canvas.drawColor(colorScheme.backgroundColor, BlendMode.srcATop);
-  }
-
   @override
   bool shouldRepaint(covariant _BaseMapPainter oldDelegate) =>
       oldDelegate.state != state ||
       oldDelegate.colorScheme != colorScheme ||
-      oldDelegate.onlyBorder != onlyBorder ||
+      oldDelegate.drawPolyline != drawPolyline ||
       oldDelegate.maps != maps;
 }
