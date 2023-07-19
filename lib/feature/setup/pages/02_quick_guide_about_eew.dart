b311861569e2cf956dad8855d567a900c376a626
YumNumm <onory0917@gmail.com>
2023-07-19 18:20:21 +0900

    fix: 初期設定画面の改善

--- lib/feature/setup/pages/02_quick_guide_about_eew.dart
+++ lib/feature/setup/pages/02_quick_guide_about_eew.dart
@@ -10,47 +10,43 @@ class QuickGuideAboutEewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Scaffold(
-      body: SetupBackgroundImageWidget(
-        child: Column(
-          children: [
-            // 画面上部のタイトル
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Text(
-                '4つの注意事項',
-                style: theme.textTheme.titleLarge!.copyWith(
-                  fontWeight: FontWeight.w900,
-                  color: Colors.white,
-                ),
-              ),
+    return Column(
+      children: [
+        // 画面上部のタイトル
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Text(
+            '4つの注意事項',
+            style: theme.textTheme.titleLarge!.copyWith(
+              fontWeight: FontWeight.w900,
+              color: Colors.white,
             ),
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: Text(
-                'EQMonitorでは、緊急地震速報をはじめとする様々な地震情報をお伝えします。\n'
-                'ただし、以下の点に注意してください。',
-                style: theme.textTheme.titleMedium!.copyWith(
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 16),
-              child: EarthquakeRestrictionWidget(),
-            ),
-            const Spacer(),
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: ActionButton.text(
-                context: context,
-                text: '次へ',
-                onPressed: onNext,
-              ),
-            ),
-          ],
+          ),
         ),
-      ),
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Text(
+            'EQMonitorでは、緊急地震速報をはじめとする様々な地震情報をお伝えします。\n'
+            'ただし、以下の点に注意してください。',
+            style: theme.textTheme.titleMedium!.copyWith(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(vertical: 16),
+          child: EarthquakeRestrictionWidget(),
+        ),
+        const Spacer(),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: ActionButton.text(
+            context: context,
+            text: '次へ',
+            onPressed: onNext,
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/feature/setup/pages/03_kmoni_warn.dart
+++ lib/feature/setup/pages/03_kmoni_warn.dart
@@ -1,90 +1,78 @@
-import 'package:collection/collection.dart';
 import 'package:eqmonitor/core/component/button/action_button.dart';
-import 'package:eqmonitor/core/component/container/bordered_container.dart';
-import 'package:eqmonitor/feature/setup/component/background_image.dart';
+import 'package:eqmonitor/core/component/widget/kmoni_caution.dart';
+import 'package:eqmonitor/feature/home/features/kmoni/viewmodel/kmoni_view_settings.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class KmoniWarnPage extends StatelessWidget {
+class KmoniWarnPage extends ConsumerWidget {
   const KmoniWarnPage({required this.onNext, super.key});
   final void Function() onNext;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final theme = Theme.of(context);
-    return Scaffold(
-      body: SetupBackgroundImageWidget(
-        child: Column(
-          children: [
-            // 画面上部のタイトル
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Text(
-                '強震モニタについて',
-                style: theme.textTheme.titleLarge!.copyWith(
-                  fontWeight: FontWeight.w900,
-                  color: Colors.white,
-                ),
-              ),
+    return Column(
+      children: [
+        // 画面上部のタイトル
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Text(
+            '強震モニタを利用しますか?',
+            style: theme.textTheme.titleLarge!.copyWith(
+              fontWeight: FontWeight.w900,
+              color: Colors.white,
             ),
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: Text(
-                'EQMonitorでは、国立研究開発法人防災科学技術研究所(防災科研)が運用する強震モニタを利用しています。\n'
-                '本アプリの利用に際して、以下の注意を確認してください',
-                style: theme.textTheme.titleMedium!.copyWith(
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            Column(
-              children: [
-                '防災科研へ本アプリの問い合わせを行わないでください',
-                '強震モニタは、揺れの様子を直感的に捉えることを目的としています',
-                '強震モニタではリアルタイムで観測値を処理しているため、ノイズや障害により観測値が変動する可能性があります',
-              ]
-                  .mapIndexed(
-                    (index, e) => BorderedContainer(
-                      accentColor:
-                          index == 0 ? Colors.redAccent.withOpacity(0.2) : null,
-                      child: Row(
-                        children: [
-                          const DecoratedBox(
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.white,
-                            ),
-                            child: SizedBox(
-                              width: 8,
-                              height: 8,
-                            ),
-                          ),
-                          const SizedBox(width: 16),
-                          Flexible(
-                            child: Text(
-                              e,
-                              style: theme.textTheme.bodyMedium!.copyWith(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                  .toList(),
-            ),
-            const Spacer(),
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: ActionButton.text(
-                context: context,
-                text: '次へ',
-                onPressed: onNext,
-              ),
-            )
-          ],
+          ),
         ),
-      ),
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Text(
+            'EQMonitorでは、国立研究開発法人防災科学技術研究所(防災科研)が運用する強震モニタを利用できます。\n'
+            '以下の注意事項をご確認いただき、利用可否を選択してください。',
+            style: theme.textTheme.titleMedium!.copyWith(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        const Flexible(
+          child: SingleChildScrollView(
+            child: KmoniCautionWidget(),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            children: [
+              Expanded(
+                child: ActionButton.text(
+                  context: context,
+                  accentColor: Colors.grey[800],
+                  text: '利用しない',
+                  onPressed: () {
+                    ref.read(kmoniSettingsProvider.notifier).setUseKmoni(
+                          value: false,
+                        );
+                    onNext();
+                  },
+                ),
+              ),
+              const SizedBox(width: 8),
+              Expanded(
+                child: ActionButton.text(
+                  context: context,
+                  text: '利用する',
+                  onPressed: () {
+                    ref.read(kmoniSettingsProvider.notifier).setUseKmoni(
+                          value: true,
+                        );
+                    onNext();
+                  },
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
