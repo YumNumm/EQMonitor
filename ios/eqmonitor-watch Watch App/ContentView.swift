7857e3c45a2128f06e2bbd5587910130abdc4fa0
YumNumm <onory0917@gmail.com>
2023-07-25 14:31:21 +0900

    add: watchos support

---
+++ ios/eqmonitor-watch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eqmonitor-watch Watch App
+//
+//  Created by 尾上 遼太朗 on 2023/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/eqmonitor-watch Watch App/eqmonitor_watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  eqmonitor_watchApp.swift
+//  eqmonitor-watch Watch App
+//
+//  Created by 尾上 遼太朗 on 2023/07/25.
+//
+
+import SwiftUI
+
+@main
+struct eqmonitor_watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
