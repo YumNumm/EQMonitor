c5b4c226d6c7c65a9b8254ff4dceec07ee2fc35a
YumNumm <onory0917@gmail.com>
2023-07-27 01:36:26 +0900

    add: melos config

--- packages/topojson/lib/src/topo_json_transform.g.dart
+++ packages/topojson/lib/src/topo_json_transform.g.dart
@@ -1,5 +1,7 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
+// ignore_for_file: type=lint
+
 part of 'topo_json_transform.dart';
 
 // **************************************************************************
@@ -7,13 +9,24 @@ part of 'topo_json_transform.dart';
 // **************************************************************************
 
 _$_TopoJsonTransform _$$_TopoJsonTransformFromJson(Map<String, dynamic> json) =>
-    _$_TopoJsonTransform(
-      scale: (json['scale'] as List<dynamic>)
-          .map((e) => (e as num).toDouble())
-          .toList(),
-      translate: (json['translate'] as List<dynamic>)
-          .map((e) => (e as num).toDouble())
-          .toList(),
+    $checkedCreate(
+      r'_$_TopoJsonTransform',
+      json,
+      ($checkedConvert) {
+        final val = _$_TopoJsonTransform(
+          scale: $checkedConvert(
+              'scale',
+              (v) => (v as List<dynamic>)
+                  .map((e) => (e as num).toDouble())
+                  .toList()),
+          translate: $checkedConvert(
+              'translate',
+              (v) => (v as List<dynamic>)
+                  .map((e) => (e as num).toDouble())
+                  .toList()),
+        );
+        return val;
+      },
     );
 
 Map<String, dynamic> _$$_TopoJsonTransformToJson(
