94709ed02f5f7ee34b71eec816b37c6cc36c4cc5
YumNumm <onory0917@gmail.com>
2023-08-28 03:49:08 +0900

    add: `LatLng`に距離を計算するExtを追加

--- packages/lat_lng/lib/src/lat_lng.dart
+++ packages/lat_lng/lib/src/lat_lng.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 
+import 'package:latlong2/latlong.dart' as lat_long2;
+
 class LatLng extends Point<double> {
   const LatLng(this.lat, this.lon) : super(lat, lon);
   factory LatLng.fromJson(Map<String, dynamic> json) {
@@ -21,4 +23,17 @@ class LatLng extends Point<double> {
         'lat': lat,
         'lon': lon,
       };
+}
+
+extension LatLngEx on LatLng {
+  /// 2点間の距離をメートルで返す
+  /// [other] 距離を測る対象の座標
+  double distanceTo(LatLng other) {
+    const distance = lat_long2.Distance();
+    return distance.as(
+      lat_long2.LengthUnit.Meter,
+      lat_long2.LatLng(lat, lon),
+      lat_long2.LatLng(other.lat, other.lon),
+    );
+  }
 }
