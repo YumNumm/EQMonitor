d4fb5ce1aa99773e2cb6c73438cbe2f8e889d494
YumNumm <onory0917@gmail.com>
2023-10-05 15:23:46 +0900

    add: maplibre

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/r_onoue/fvm/versions/3.13.2"
-export "FLUTTER_APPLICATION_PATH=/Users/r_onoue/dev/EQMonitor"
+export "FLUTTER_ROOT=/Users/r_onoue/fvm/versions/3.13.3"
+export "FLUTTER_APPLICATION_PATH=/Users/r_onoue/dev/github.com/YumNumm/EQMonitor"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/r_onoue/dev/github.com/YumNumm/EQMonitor/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=2.1.0"
 export "FLUTTER_BUILD_NUMBER=2.1.0"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9iOGQzNTgxMGU5MWFiOGZjMzliYTVlN2E0MWJmZjZmNjk3ZThlM2E4Lw=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/r_onoue/dev/github.com/YumNumm/EQMonitor/.dart_tool/package_config.json"
--- packages/eqapi_schema/example/eqapi_schema_example.dart
+++ packages/eqapi_schema/example/eqapi_schema_example.dart
@@ -14,7 +14,7 @@ Future<List<TelegramV3>> main() async {
     final telegrams = value as List<dynamic>;
     for (final e in telegrams) {
       final telegram = TelegramV3.fromJsonAndEventID(
-          e as Map<String, dynamic>, int.parse(eventId));
+          e as Map<String, dynamic>, int.parse(eventId),);
       results.add(telegram);
     }
   });
