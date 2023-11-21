45ca4c1c748542fc19e4c09b65121d9d711d7ac3
YumNumm <onory0917@gmail.com>
2023-11-21 22:40:30 +0900

    remove: beta in appbar

--- lib/feature/home/view/home_view.dart
+++ lib/feature/home/view/home_view.dart
@@ -20,7 +20,6 @@ import 'package:eqmonitor/feature/home/features/kmoni/viewmodel/kmoni_view_model
 import 'package:eqmonitor/feature/home/features/kmoni/viewmodel/kmoni_view_settings.dart';
 import 'package:eqmonitor/feature/home/features/kmoni/widget/kmoni_maintenance_widget.dart';
 import 'package:eqmonitor/feature/home/viewmodel/home_viewmodel.dart';
-import 'package:eqmonitor/gen/fonts.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
@@ -64,25 +63,11 @@ class HomeView extends HookConsumerWidget {
                 }
               }
             },
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.baseline,
-              textBaseline: TextBaseline.alphabetic,
-              children: [
-                Text(
-                  'EQMonitor',
-                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                        fontWeight: FontWeight.bold,
-                      ),
-                ),
-                const SizedBox(width: 4),
-                Text(
-                  'Beta',
-                  style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                        fontFamily: FontFamily.jetBrainsMono,
-                      ),
-                ),
-              ],
+            child: Text(
+              'EQMonitor',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
             ),
           ),
           forceMaterialTransparency: true,
