4ba89a87e326b66f0037ccdb2d8d26c474bec8b1
YumNumm <onory0917@gmail.com>
2023-11-25 02:08:35 +0900

    add: EEW FCMの仮追加

--- lib/feature/settings/children/config/notification/children/eew_notification_settings_page.dart
+++ lib/feature/settings/children/config/notification/children/eew_notification_settings_page.dart
@@ -1,4 +1,5 @@
 import 'package:eqmonitor/core/component/container/bordered_container.dart';
+import 'package:eqmonitor/core/provider/config/notification/fcm_topic_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -12,13 +13,22 @@ class EewNotificationSettingsPage extends ConsumerWidget {
         title: const Text('緊急地震速報 通知設定'),
       ),
       body: ListView(
-        children: const [
-          _InformationCard(
+        children: [
+          const _InformationCard(
             icon: Icon(Icons.info),
             title: '緊急地震速報通知についての注意',
             description: '現在、重大な通知には対応していません。マナーモード時には、鳴動しません。\n'
                 'また、現在地に基づいた通知・震度4以下の詳細な通知条件分岐には対応していません。\n'
                 '詳しくは、ロードマップをご覧ください。',
+          ),
+          FloatingActionButton.extended(
+            onPressed: () async {
+              await ref
+                  .read(fcmTopicManagerProvider.notifier)
+                  .registerToTopic(const FcmEewAllTopic());
+            },
+            label: const Text('EEW ALLを購読する'),
+            icon: const Icon(Icons.add),
           ),
         ],
       ),
