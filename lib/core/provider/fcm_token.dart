dcad6b5e6edc2c1fbc50bca0c0b8a1df48316759
Ryotaro Onoue <73390859+YumNumm@users.noreply.github.com>
2023-12-04 23:31:38 +0900

    add: デバッグ時にFCM・APNS Tokenを表示するように (#434)

--- lib/core/provider/fcm_token.dart
+++
@@ -1,5 +0,0 @@
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-final fcmTokenProvider = Provider<String>(
-  (ref) => 'FCM Token is not provided',
-);
---
+++ lib/core/provider/notification_token.dart
@@ -0,0 +1,31 @@
+import 'package:eqmonitor/core/provider/firebase/firebase_messaging.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'notification_token.freezed.dart';
+part 'notification_token.g.dart';
+
+@riverpod
+Future<NotificationTokenModel> notificationToken(NotificationTokenRef ref) async {
+  final messaging = ref.watch(firebaseMessagingProvider);
+  final results = await (
+    messaging.getAPNSToken(),
+    messaging.getToken(),
+  ).wait;
+
+  return NotificationTokenModel(
+    apnsToken: results.$1,
+    fcmToken: results.$2,
+  );
+}
+
+@freezed
+class NotificationTokenModel with _$NotificationTokenModel {
+  const factory NotificationTokenModel({
+    required String? fcmToken,
+    required String? apnsToken,
+  }) = _NotificationTokenModel;
+
+  factory NotificationTokenModel.fromJson(Map<String, dynamic> json) =>
+      _$NotificationTokenModelFromJson(json);
+}
