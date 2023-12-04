f8f3d8ce217a8cf92864414ee02fc01a139388ec
Ryotaro Onoue <73390859+YumNumm@users.noreply.github.com>
2023-12-04 20:34:40 +0900

    [iOS] 通知画像が表示されない問題を修正 (#433)

---
+++ ios/FcmServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  FcmServiceExtension
+//
+//  Created by 尾上 遼太朗 on 2023/12/04.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
