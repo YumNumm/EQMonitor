c7cb10812aa6004e05b9fb261add35bf66fc1bc1
YumNumm <onory0917@gmail.com>
2023-12-05 00:14:36 +0900

    add: library

---
+++ ios/FcmServiceExtension/NotificationService.swift
@@ -0,0 +1,7 @@
+import UserNotifications
+import awesome_notifications_fcm
+
+@available(iOS 10.0, *)
+class NotificationService: DartAwesomeServiceExtension {
+
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,8 @@ import FlutterMacOS
 import Foundation
 
 import appkit_ui_element_colors
+import awesome_notifications
+import awesome_notifications_core
 import device_info_plus
 import dynamic_color
 import firebase_analytics
@@ -24,6 +26,8 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppkitUiElementColorsPlugin.register(with: registry.registrar(forPlugin: "AppkitUiElementColorsPlugin"))
+  AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
+  AwesomeNotificationsCorePlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsCorePlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
