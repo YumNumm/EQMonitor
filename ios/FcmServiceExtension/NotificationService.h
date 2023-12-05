514b86d2c3faba0abe5133c70a72ebb5d3023c44
YumNumm <onory0917@gmail.com>
2023-12-05 23:03:22 +0900

    fix: [ios] build error

--- ios/FcmServiceExtension/NotificationService.h
+++ ios/FcmServiceExtension/NotificationService.h
@@ -2,7 +2,7 @@
 //  NotificationService.h
 //  FcmServiceExtension
 //
-//  Created by 尾上 遼太朗 on 2023/12/04.
+//  Created by 尾上 遼太朗 on 2023/12/05.
 //
 
 #import <UserNotifications/UserNotifications.h>
