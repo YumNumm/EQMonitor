73f443c66c291892e6cc2157a60f77070fd64dfa
YumNumm <onory0917@gmail.com>
2023-12-28 19:41:35 +0900

    add

---
+++ lib/core/provider/config/earthquake_history/earthquake_history_config_provider.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint, duplicate_ignore
+
+part of 'earthquake_history_config_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$earthquakeHistoryConfigHash() =>
+    r'b1fdfbaa45d3af3f74336154f5d1f905bd84e51c';
+
+/// See also [EarthquakeHistoryConfig].
+@ProviderFor(EarthquakeHistoryConfig)
+final earthquakeHistoryConfigProvider = AutoDisposeNotifierProvider<
+    EarthquakeHistoryConfig, EarthquakeHistoryConfigModel>.internal(
+  EarthquakeHistoryConfig.new,
+  name: r'earthquakeHistoryConfigProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$earthquakeHistoryConfigHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$EarthquakeHistoryConfig
+    = AutoDisposeNotifier<EarthquakeHistoryConfigModel>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
