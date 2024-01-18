ea7d50391747511c02e36091f3fd9011a63bc7a6
Restyled.io <commits@restyled.io>
2024-01-18 12:20:53 +0000

    Restyled by shellcheck

--- packages/jma_parameter_converter_internal/updater.sh
+++ packages/jma_parameter_converter_internal/updater.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 rm earthquake*
 rm tsunami*
-curl "https://api.dmdata.jp/v2/parameter/earthquake/station" -H "authorization: Basic $(echo $DMDATA_TOKEN: | base64)" > earthquake_parameter.json
-curl "https://api.dmdata.jp/v2/parameter/tsunami/station" -H "authorization: Basic $(echo $DMDATA_TOKEN: | base64)" > tsunami_parameter.json
+curl "https://api.dmdata.jp/v2/parameter/earthquake/station" -H "authorization: Basic $(echo "$DMDATA_TOKEN": | base64)" > earthquake_parameter.json
+curl "https://api.dmdata.jp/v2/parameter/tsunami/station" -H "authorization: Basic $(echo "$DMDATA_TOKEN": | base64)" > tsunami_parameter.json
 fvm dart run ./bin/jma_parameter_converter_internal.dart earthquake_station.json
 fvm dart run ./bin/jma_parameter_converter_internal.dart tsunami_station.json
