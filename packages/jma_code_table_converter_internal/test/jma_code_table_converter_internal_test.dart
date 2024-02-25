0230fb613b6c6eb86c445ff65f7fcbdc0fd91a59
YumNumm <onory0917@gmail.com>
2024-02-25 17:32:58 +0900

    add: converter

--- packages/jma_code_table_converter_internal/test/jma_code_table_converter_internal_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:jma_code_table_converter_internal/jma_code_table_converter_internal.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
