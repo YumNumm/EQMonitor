c828c7725f9f093b5b23672ec0491ce228cd300d
Restyled.io <commits@restyled.io>
2024-04-04 23:42:31 +0000

    Restyled by shfmt

--- packages/kyoshin_observation_point_types/util/update_proto.sh
+++ packages/kyoshin_observation_point_types/util/update_proto.sh
@@ -2,12 +2,12 @@
 
 # ensure protoc-gen-dart is in path
 if ! type -P protoc-gen-dart &>/dev/null; then
-    echo "protoc-gen-dart not found in PATH"
-    exit 1
+  echo "protoc-gen-dart not found in PATH"
+  exit 1
 fi
 
 rm -rf lib/*
 protoc \
-    --dart_out="grpc:lib" \
-    -I="./proto" \
-    "$(find . -iname "*.proto")"
+  --dart_out="grpc:lib" \
+  -I="./proto" \
+  "$(find . -iname "*.proto")"
