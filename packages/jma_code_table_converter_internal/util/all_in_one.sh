17816766c0170c7442133cf848db0506adcc1d9a
Restyled.io <commits@restyled.io>
2024-05-29 18:59:32 +0000

    Restyled by shfmt

--- packages/jma_code_table_converter_internal/util/all_in_one.sh
+++ packages/jma_code_table_converter_internal/util/all_in_one.sh
@@ -4,20 +4,20 @@ mkdir tmp
 
 echo "------ Downloading base XLS sheet ------"
 wget https://xml.kishou.go.jp/jmaxml_20240216_Code.zip \
-    -O tmp/jmaxml_20240216_Code.zip \
-    --no-check-certificate \
-    --quiet
+  -O tmp/jmaxml_20240216_Code.zip \
+  --no-check-certificate \
+  --quiet
 echo "unzipping..."
-unzip -O sjis tmp/jmaxml_20240216_Code.zip -d tmp > /dev/null
+unzip -O sjis tmp/jmaxml_20240216_Code.zip -d tmp >/dev/null
 
 echo "------ Converting XLS to CSV ------"
 cd util/xls_to_csv || exit
 echo "syncing rye..."
-rye sync > /dev/null
+rye sync >/dev/null
 echo "converting..."
 rye run python3 src/xls_to_csv/main.py \
-   -i ../../tmp/地震火山関連コード表.xls \
-   -o ../../tmp/output
+  -i ../../tmp/地震火山関連コード表.xls \
+  -o ../../tmp/output
 cd ../..
 
 echo "------ Generating code table ------"
