62a4b8976999afc93dd2b777fdacf39a511f86bf
Ryotaro Onoue <73390859+YumNumm@users.noreply.github.com>
2024-05-31 04:53:05 +0900

    [FEATURE] 配色切り替えの実装 (#710)
    * add: 配色設定
    
    * fix

---
+++ app/lib/gen/fonts.gen.dart
@@ -0,0 +1,18 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class FontFamily {
+  FontFamily._();
+
+  /// Font family: JetBrainsMono
+  static const String jetBrainsMono = 'JetBrainsMono';
+
+  /// Font family: NotoSansJP
+  static const String notoSansJP = 'NotoSansJP';
+}
