6d6836170bbab71e3b4faee26f0adaa197c3662d
YumNumm <onory0917@gmail.com>
2024-09-12 03:12:05 +0900

    add: current location

--- app/lib/feature/home/features/map/viewmodel/main_map_viewmodel.dart
+++ app/lib/feature/home/features/map/viewmodel/main_map_viewmodel.dart
@@ -139,6 +139,9 @@ class MainMapViewModel extends _$MainMapViewModel {
         _EewEstimatedIntensityCalculatedCityService(
       controller: controller,
     );
+    _currentLocationIconService = _CurrentLocationIconService(
+      controller: controller,
+    );
 
     await (
       _kmoniObservationPointService!.init(),
@@ -161,6 +164,7 @@ class MainMapViewModel extends _$MainMapViewModel {
       hypocenterLowPreciseIcon:
           ref.read(hypocenterLowPreciseIconRenderProvider)!,
     );
+    await _currentLocationIconService!.init();
 
     // 地図の移動を監視
     controller.addListener(() {
@@ -178,6 +182,7 @@ class MainMapViewModel extends _$MainMapViewModel {
         _eewPsWaveService!.dispose(),
         _eewEstimatedIntensityService.dispose(),
         _shakeDetectionBorderService!.dispose(),
+        _currentLocationIconService!.dispose(),
       ).wait;
     });
     log('_onEewStateChanged called!', name: 'MainMapViewModel');
@@ -220,6 +225,8 @@ class MainMapViewModel extends _$MainMapViewModel {
 
   _EewEstimatedIntensityCalculatedRegionService?
       _eewEstimatedIntensityCalculatedRegionService;
+
+  _CurrentLocationIconService? _currentLocationIconService;
 
   Future<void> _onEewStateChanged(List<EewV1> values) async {
     // 初期化が終わっていない場合は何もしない
@@ -338,6 +345,11 @@ class MainMapViewModel extends _$MainMapViewModel {
       ref.read(eewAliveTelegramProvider) ?? [],
     );
   }
+
+  Future<void> onLocationChanged(double lat, double lng) async =>
+      _currentLocationIconService?.update(
+        (lat, lng),
+      );
 
   // *********** Utilities ***********
   Future<void> updateImage({
@@ -1542,7 +1554,18 @@ class _CurrentLocationIconService {
       layerId,
       const SymbolLayerProperties(
         iconImage: 'current-location',
+        iconSize: [
+          'interpolate',
+          ['linear'],
+          ['zoom'],
+          3,
+          0.1,
+          20,
+          1,
+        ],
+        iconAllowOverlap: true,
       ),
+      sourceLayer: layerId,
     );
   }
 
--- app/lib/feature/location/data/location.g.dart
+++ app/lib/feature/location/data/location.g.dart
@@ -8,7 +8,7 @@ part of 'location.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$locationStreamHash() => r'3615fa0623715cabbad0f2bc73ad080071413eb9';
+String _$locationStreamHash() => r'e4989c7c7324f98829b1f655bb9aadb77dd5ef19';
 
 /// See also [locationStream].
 @ProviderFor(locationStream)
@@ -23,5 +23,23 @@ final locationStreamProvider = AutoDisposeStreamProvider<Position>.internal(
 );
 
 typedef LocationStreamRef = AutoDisposeStreamProviderRef<Position>;
+String _$closestKmoniObservationPointStreamHash() =>
+    r'9ed9db9ec44171fb92f9f211d659cbc490538f43';
+
+/// See also [closestKmoniObservationPointStream].
+@ProviderFor(closestKmoniObservationPointStream)
+final closestKmoniObservationPointStreamProvider =
+    AutoDisposeStreamProvider<(KyoshinObservationPoint, double km)>.internal(
+  closestKmoniObservationPointStream,
+  name: r'closestKmoniObservationPointStreamProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$closestKmoniObservationPointStreamHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ClosestKmoniObservationPointStreamRef
+    = AutoDisposeStreamProviderRef<(KyoshinObservationPoint, double km)>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, inference_failure_on_uninitialized_variable, inference_failure_on_function_return_type, inference_failure_on_untyped_parameter, deprecated_member_use_from_same_package
