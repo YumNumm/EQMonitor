2941936afcf5002f769082d0af399edacab0381f
YumNumm <onory0917@gmail.com>
2025-02-10 05:20:45 +0900

    refactor: 地図レイヤー関連のコード整理

--- app/lib/feature/map/data/layer/base/map_layer.dart
+++ app/lib/feature/map/data/layer/base/map_layer.dart
@@ -1,14 +1,11 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:maplibre_gl/maplibre_gl.dart';
-
-part 'map_layer.freezed.dart';
 
 abstract class MapLayer {
   const MapLayer();
 
-  Map<String, dynamic> toGeoJsonSource();
+  Map<String, dynamic>? toGeoJsonSource();
   String get geoJsonSourceHash;
-  LayerProperties toLayerProperties();
+  LayerProperties? toLayerProperties();
   String get layerPropertiesHash;
 
   String get id;
@@ -17,27 +14,4 @@ abstract class MapLayer {
   double? get minZoom;
   double? get maxZoom;
   dynamic get filter;
-}
-
-@freezed
-class CachedMapLayer with _$CachedMapLayer {
-  const factory CachedMapLayer({
-    required MapLayer layer,
-    required String geoJsonSourceHash,
-    required String layerPropertiesHash,
-    required String? belowLayerId,
-  }) = _CachedMapLayer;
-
-  const CachedMapLayer._();
-
-  factory CachedMapLayer.fromLayer({
-    required MapLayer layer,
-    String? belowLayerId,
-  }) =>
-      CachedMapLayer(
-        layer: layer,
-        geoJsonSourceHash: layer.geoJsonSourceHash,
-        layerPropertiesHash: layer.layerPropertiesHash,
-        belowLayerId: belowLayerId,
-      );
 }
