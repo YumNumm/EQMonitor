adcece57206b09ebd9798dbba609b0dc84c97f35
YumNumm <onory0917@gmail.com>
2025-02-28 23:18:54 +0900

    remove unused widgets

--- app/lib/feature/home/ui/component/map/home_map_view.dart
+++ app/lib/feature/home/ui/component/map/home_map_view.dart
@@ -1,72 +1,28 @@
-import 'package:eqmonitor/core/utils/map_camera_position_helper.dart';
-import 'package:eqmonitor/feature/home/ui/component/map/home_map_content.dart';
 import 'package:eqmonitor/feature/home/ui/component/map/home_map_layer_modal.dart';
 import 'package:eqmonitor/feature/home/ui/component/map/kyoshin_monitor_scale_card.dart';
 import 'package:eqmonitor/feature/kyoshin_monitor/data/provider/kyoshin_monitor_settings.dart';
 import 'package:eqmonitor/feature/kyoshin_monitor/page/components/kyoshin_monitor_status_card.dart';
 import 'package:eqmonitor/feature/kyoshin_monitor/page/kyoshin_monitor_settings_modal.dart';
-import 'package:eqmonitor/feature/map/data/controller/declarative_map_controller.dart';
-import 'package:eqmonitor/feature/map/data/model/camera_position.dart';
 import 'package:eqmonitor/feature/map/ui/components/controller/map_layer_controller_card.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:maplibre_gl/maplibre_gl.dart';
 
 class HomeMapView extends HookConsumerWidget {
   const HomeMapView({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final mapController = useMemoized(
-      DeclarativeMapController.new,
-    );
-
-    final size = MediaQuery.sizeOf(context);
-    final cameraPosition = useMemoized(
-      () => MapCameraPosition(
-        target:
-            MapCameraPositionHelper.calculateJapanCenterPosition(
-              size.width,
-              size.height,
-            ),
-        zoom:
-            MapCameraPositionHelper.calculateJapanZoomLevel(
-              size.width,
-              size.height,
-            ),
+    return const SafeArea(
+      child: Padding(
+        padding: EdgeInsets.all(8),
+        child: _MapHeader(),
       ),
-      [size.width, size.height],
-    );
-
-    return Stack(
-      children: [
-        HomeMapContent(
-          mapController: mapController,
-          cameraPosition: cameraPosition,
-        ),
-        SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(8),
-            child: _MapHeader(
-              mapController: mapController,
-              size: size,
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
 
 class _MapHeader extends ConsumerWidget {
-  const _MapHeader({
-    required this.mapController,
-    required this.size,
-  });
-
-  final DeclarativeMapController mapController;
-  final Size size;
+  const _MapHeader();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -127,22 +83,7 @@ class _MapHeader extends ConsumerWidget {
         MapLayerControllerCard(
           onLayerButtonTap:
               () async => HomeMapLayerModal.show(context),
-          onLocationButtonTap: () async {
-            await mapController.moveCameraToPosition(
-              CameraPosition(
-                target:
-                    MapCameraPositionHelper.calculateJapanCenterPosition(
-                      size.width,
-                      size.height,
-                    ),
-                zoom:
-                    MapCameraPositionHelper.calculateJapanZoomLevel(
-                      size.width,
-                      size.height,
-                    ),
-              ),
-            );
-          },
+          onLocationButtonTap: () async {},
         ),
       ],
     );
