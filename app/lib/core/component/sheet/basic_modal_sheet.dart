08f275c07d92e0e5ec183d7e7cfb10889607c19f
Ryotaro Onoue (A2442) <admin@yumnumm.dev>
2025-03-28 00:15:18 +0900

    chore: 共通モーダルのデザイン更新

--- app/lib/core/component/sheet/basic_modal_sheet.dart
+++ app/lib/core/component/sheet/basic_modal_sheet.dart
@@ -4,65 +4,67 @@ import 'package:sheet/sheet.dart';
 
 class BasicModalSheet extends HookWidget {
   const BasicModalSheet({
-    required this.children,
+    required this.child,
     super.key,
     this.hasAppBar = true,
   });
 
-  final List<Widget> children;
+  final Widget child;
   final bool hasAppBar;
-
-  static double? width(Size size) {
-    return size.width > 600 && size.height < size.width ? size.width / 2 : null;
-  }
-
-  static const double kSheetBorderRadius = 28;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final colorScheme = theme.colorScheme;
 
-    final size = MediaQuery.sizeOf(context);
-
-    final sheetWidth = width(size);
-
-    final sheet = Stack(
-      children: [
-        Sheet(
-          backgroundColor: Colors.transparent,
-          initialExtent: size.height * 0.2,
-          physics: const SnapSheetPhysics(stops: <double>[0.2, 0.5, 1]),
-          child: DecoratedBox(
-            decoration: BoxDecoration(
+    return SafeArea(
+      bottom: false,
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          final size = (
+            width: constraints.maxWidth,
+            height: constraints.maxHeight,
+          );
+          final isLandscape = size.width > size.height;
+          final sheet = Sheet(
+            backgroundColor: colorScheme.surface,
+            shape: RoundedRectangleBorder(
               borderRadius: const BorderRadius.vertical(
-                top: Radius.circular(kSheetBorderRadius),
+                top: Radius.circular(16),
               ),
-              color: theme.colorScheme.surfaceContainerLowest,
-              border: Border.all(
-                color: theme.colorScheme.onSurface.withValues(alpha: 0.1),
+              side: BorderSide(color: colorScheme.outlineVariant, width: 0),
+            ),
+            initialExtent: size.height * 0.2,
+            physics: const SnapSheetPhysics(stops: [0.1, 0.2, 0.5, 0.8, 1]),
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 8),
+                  width: 36,
+                  height: 4,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: theme.colorScheme.onSurface,
+                  ),
+                ),
+                child,
+              ],
+            ),
+          );
+
+          if (isLandscape) {
+            return Align(
+              alignment: Alignment.centerRight,
+              child: SizedBox(
+                width: size.width * 0.5,
+                height: size.height,
+                child: sheet,
               ),
-            ),
-            child: SafeArea(
-              top: hasAppBar,
-              bottom: false,
-              child: SingleChildScrollView(child: Column(children: children)),
-            ),
-          ),
-        ),
-      ],
-    );
-    if (sheetWidth == null) {
-      return sheet;
-    }
-    return Align(
-      alignment: Alignment.bottomRight,
-      child: SafeArea(
-        top: hasAppBar,
-        bottom: false,
-        child: Padding(
-          padding: const EdgeInsets.only(right: 8),
-          child: SizedBox(width: sheetWidth, child: sheet),
-        ),
+            );
+          }
+          return sheet;
+        },
       ),
     );
   }
