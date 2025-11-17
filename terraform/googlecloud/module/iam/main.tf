143b444274e41fd5c86065fdb82bf9aaaefadeb5
YumNumm <admin@yumnumm.dev>
2025-11-18 03:42:27 +0900

    add: SA
    Signed-off-by: YumNumm <admin@yumnumm.dev>

--- terraform/googlecloud/module/iam/main.tf
+++ terraform/googlecloud/module/iam/main.tf
@@ -1,16 +1,32 @@
-resource "google_service_account" "github_actions_eqmonitor_service_account" {
-  account_id   = "actions-eqmonitor"
+resource "google_service_account" "github_actions_app_distro_service_account" {
+  account_id   = "actions-eqmonitor-appdistro"
   display_name = "GitHub Actions EQMonitor Service Account"
   project      = var.project_id
 }
 
-resource "google_project_iam_member" "github_actions_eqmonitor_app_distro_admin_member" {
+resource "google_project_iam_member" "github_actions_app_distro_admin_member" {
   project = var.project_id
   # See: https://firebase.google.com/docs/projects/iam/roles-predefined-product?hl=ja#app-distro
   role   = "roles/firebaseappdistro.admin"
-  member = "serviceAccount:${google_service_account.github_actions_eqmonitor_service_account.email}"
+  member = "serviceAccount:${google_service_account.github_actions_app_distro_service_account.email}"
 }
 
-output "github_actions_eqmonitor_service_account_name" {
-  value = google_service_account.github_actions_eqmonitor_service_account.name
+output "github_actions_app_distro_service_account_name" {
+  value = google_service_account.github_actions_app_distro_service_account.name
+}
+
+
+resource "google_service_account" "github_actions_writer_service_account" {
+  account_id   = "actions-eqmonitor-writer"
+  display_name = "GitHub Actions EQMonitor Service Account"
+  project      = var.project_id
+}
+resource "google_project_iam_member" "github_actions_writer_member" {
+  project = var.project_id
+  role   = "roles/writer"
+  member = "serviceAccount:${google_service_account.github_actions_writer_service_account.email}"
+}
+
+output "github_actions_writer_service_account_name" {
+  value = google_service_account.github_actions_writer_service_account.name
 }
