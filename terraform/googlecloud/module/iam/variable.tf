7d6a517350987e82fe67a789c64b26a7b8b4e41b
YumNumm <admin@yumnumm.dev>
2025-11-18 03:26:08 +0900

    test
    Signed-off-by: YumNumm <admin@yumnumm.dev>

--- terraform/googlecloud/module/iam/variable.tf
+++ terraform/googlecloud/module/iam/variable.tf
@@ -1,8 +1,3 @@
-variable "prefix" {
-  description = "Prefix for resource names"
-  type        = string
-}
-
 variable "project_id" {
   description = "Google Cloud Project ID"
   type        = string
