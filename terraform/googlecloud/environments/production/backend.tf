76c080a7f182257dc504d6cb6e4effd03b327984
YumNumm <admin@yumnumm.dev>
2025-11-18 03:01:15 +0900

    tmp
    Signed-off-by: YumNumm <admin@yumnumm.dev>

---
+++ terraform/googlecloud/environments/production/backend.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = "1.13.5"
+  backend "s3" {
+    bucket                      = "eqmonitor-tf-state"
+    key                         = "eqmonitor-googlecloud-production.tfstate"
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+    endpoints                   = { s3 = "https://4270d8a14dfc1f866629099a788c93e2.r2.cloudflarestorage.com" }
+  }
+}
