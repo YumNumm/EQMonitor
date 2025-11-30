c1cb265517a29bc437e77ac2961206f283eb49ad
YumNumm <admin@yumnumm.dev>
2025-11-30 11:33:23 +0900

    update gradle version
    Signed-off-by: YumNumm <admin@yumnumm.dev>

--- app/android/settings.gradle.kts
+++ app/android/settings.gradle.kts
@@ -18,9 +18,9 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.8.0" apply false
+    id("com.android.application") version "8.13.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
     id("com.google.gms.google-services") version("4.3.15") apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
 
 include(":app")
