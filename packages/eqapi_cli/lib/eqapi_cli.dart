365d928531c3d692ed0dca91e18d5de6042872e2
YumNumm <admin@yumnumm.dev>
2026-03-01 20:18:15 +0900

    remove eqapi_cli

--- packages/eqapi_cli/lib/eqapi_cli.dart
+++
@@ -1,5 +0,0 @@
-export 'src/api_client.dart';
-export 'src/commands/earthquake_command.dart';
-export 'src/commands/eew_command.dart';
-export 'src/formatters/earthquake_formatter.dart';
-export 'src/formatters/eew_formatter.dart';
