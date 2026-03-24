2a0331dd7fd59a36401219299b59ce40507d5958
Ryotaro Onoue <admin@yumnumm.dev>
2026-03-24 19:40:51 +0900

    regenerate code
    Signed-off-by: Ryotaro Onoue <admin@yumnumm.dev>

--- app/lib/core/provider/dio_provider.g.dart
+++ app/lib/core/provider/dio_provider.g.dart
@@ -49,4 +49,4 @@ final class DioProvider extends $FunctionalProvider<Dio, Dio, Dio>
   }
 }
 
-String _$dioHash() => r'217681bb3e86b36ae21b12c5278db2f19391dc12';
+String _$dioHash() => r'7326321bb56ba3d5290153322fb9fc47f0ab5bef';
