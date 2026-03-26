2e865cc3d9cfdc7f1efe966c82f233df6a9b8699
Ryotaro Onoue <admin@yumnumm.dev>
2026-03-26 21:19:23 +0900

    update user agent
    Signed-off-by: Ryotaro Onoue <admin@yumnumm.dev>

--- app/lib/feature/kyoshin_monitor/data/api/kyoshin_monitor_dio.dart
+++ app/lib/feature/kyoshin_monitor/data/api/kyoshin_monitor_dio.dart
@@ -1,9 +1,7 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart';
-import 'package:eqmonitor/core/provider/log/talker.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:talker_flutter/talker_flutter.dart';
 
 part 'kyoshin_monitor_dio.g.dart';
 
@@ -14,37 +12,17 @@ Dio kyoshinMonitorDio(Ref ref) => Dio(
     receiveTimeout: const Duration(seconds: 2),
     sendTimeout: const Duration(seconds: 2),
     headers: {
-      HttpHeaders.userAgentHeader:
-          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
-      HttpHeaders.refererHeader: 'http://www.kmoni.bosai.go.jp/',
-      HttpHeaders.hostHeader: 'www.kmoni.bosai.go.jp',
-      HttpHeaders.cacheControlHeader: 'no-cache',
       HttpHeaders.acceptHeader:
-          'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+          'text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01',
+      'Accept-Language': 'ja-JP,ja;q=0.9,en-JP;q=0.8,en;q=0.7,en-US;q=0.6',
+      HttpHeaders.cacheControlHeader: 'no-cache',
+      'Connection': 'keep-alive',
+      'DNT': '1',
+      'Pragma': 'no-cache',
+      HttpHeaders.refererHeader: 'http://www.kmoni.bosai.go.jp/',
+      HttpHeaders.userAgentHeader:
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36',
+      'X-Requested-With': 'XMLHttpRequest',
     },
   ),
-)..interceptors.add(
-InterceptorsWrapper(
-  onError: (error, handler) {
-    talker.log(
-      'Error: ${error.response?.statusCode} ${error.response?.requestOptions.uri}',
-      pen: AnsiPen()..red(),
-    );
-    return handler.next(error);
-  },
-  onRequest: (options, handler) {
-    talker.log(
-      'Request: ${options.method} ${options.uri}',
-      pen: AnsiPen()..yellow(),
-    );
-    return handler.next(options);
-  },
-  onResponse: (response, handler) {
-    talker.log(
-      'Response: ${response.statusCode} ${response.requestOptions.uri}',
-      pen: AnsiPen()..green(),
-    );
-    return handler.next(response);
-  },
-)
 );
