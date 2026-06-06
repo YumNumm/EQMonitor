11b8c67c690b29490a4e8e53fbae6251e34daadf
Ryotaro <onory0917@gmail.com>
2026-06-06 16:58:02 +0900

    feat(eew): EEW詳細画面にシミュレーション再生機能を追加
    過去のEEWイベントの各報を実際の時間間隔で再生するシミュレーション
    機能を追加。再生/一時停止/停止の制御が可能で、時間間隔は500ms〜5sに
    クランプされる。

---
+++ app/lib/feature/eew/data/eew_simulation_notifier.g.dart
@@ -0,0 +1,64 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint, type=warning, duplicate_ignore, unused_element_parameter
+
+part of 'eew_simulation_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(EewSimulation)
+final eewSimulationProvider = EewSimulationProvider._();
+
+final class EewSimulationProvider
+    extends $NotifierProvider<EewSimulation, EewSimulationState?> {
+  EewSimulationProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'eewSimulationProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$eewSimulationHash();
+
+  @$internal
+  @override
+  EewSimulation create() => EewSimulation();
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(EewSimulationState? value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<EewSimulationState?>(value),
+    );
+  }
+}
+
+String _$eewSimulationHash() => r'42bd9f83589175c205258cb3db53c022fdae975c';
+
+abstract class _$EewSimulation extends $Notifier<EewSimulationState?> {
+  EewSimulationState? build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final ref = this.ref as $Ref<EewSimulationState?, EewSimulationState?>;
+    final element =
+        ref.element
+            as $ClassProviderElement<
+              AnyNotifier<EewSimulationState?, EewSimulationState?>,
+              EewSimulationState?,
+              Object?,
+              Object?
+            >;
+    element.handleCreate(ref, build);
+  }
+}
