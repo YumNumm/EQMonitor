14db756a4201cdf48b1a6d37e1b80532cea21178
Ryotaro <onory0917@gmail.com>
2026-06-06 20:28:38 +0900

    fix: regenerate missing retrofit implementation for TelegramApiClient
    getV2TelegramEventIdEventIdDetails was added but .g.dart was not regenerated, causing CD build failure.

--- packages/eqmonitor_api/lib/src/clients/telegram_api_client.g.dart
+++ packages/eqmonitor_api/lib/src/clients/telegram_api_client.g.dart
@@ -135,6 +135,38 @@ class _TelegramApiClient implements TelegramApiClient {
   }
 
   @override
+  Future<HttpResponse<TelegramEventDetailsResponse>>
+  getV2TelegramEventIdEventIdDetails({
+    required String eventId,
+    List<TelegramStatus> statuses = const [.normal],
+  }) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{r'statuses': statuses};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<HttpResponse<TelegramEventDetailsResponse>>(
+      Options(method: 'GET', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/v2/telegram/eventId/${eventId}/details',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, Object?>>(_options);
+    late TelegramEventDetailsResponse _value;
+    try {
+      _value = TelegramEventDetailsResponse.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options, response: _result);
+      rethrow;
+    }
+    final httpResponse = HttpResponse(_value, _result);
+    return httpResponse;
+  }
+
+  @override
   Future<HttpResponse<TelegramDetailResponse>> getV2TelegramId({
     required String id,
   }) async {
