721f69f47072bdfa5ae4b8ceafac27b9b74324ab
Ryotaro Onoue <admin@yumnumm.dev>
2026-06-23 07:51:17 +0900

    test(tsunami): 差分追跡の網羅テスト追加 ＋ meta lookupを安全化

--- app/lib/feature/tsunami/data/model/timeline/tsunami_timeline.dart
+++ app/lib/feature/tsunami/data/model/timeline/tsunami_timeline.dart
@@ -5,7 +5,17 @@ import 'package:eqmonitor/feature/tsunami/data/model/timeline/max_height_timelin
 import 'package:eqmonitor/feature/tsunami/data/model/timeline/observation_timeline_entry.dart';
 import 'package:eqmonitor/feature/tsunami/data/model/timeline/station_timeline_entry.dart';
 import 'package:eqmonitor/feature/tsunami/data/model/tracking/tracked_tsunami_timeline.dart';
+import 'package:eqmonitor/feature/tsunami/data/model/tracking/tracked_value.dart';
+import 'package:eqmonitor/feature/tsunami/data/model/tsunami_estimation_first_height.dart';
+import 'package:eqmonitor/feature/tsunami/data/model/tsunami_estimation_max_height.dart';
+import 'package:eqmonitor/feature/tsunami/data/model/tsunami_forecast_first_height.dart';
+import 'package:eqmonitor/feature/tsunami/data/model/tsunami_forecast_max_height.dart';
+import 'package:eqmonitor/feature/tsunami/data/model/tsunami_observation_first_height.dart';
+import 'package:eqmonitor/feature/tsunami/data/model/tsunami_observation_max_height.dart';
+import 'package:eqmonitor/feature/tsunami/data/model/tsunami_station_forecast.dart';
+import 'package:eqmonitor/feature/tsunami/data/model/tsunami_station_observation.dart';
 import 'package:eqmonitor/feature/tsunami/data/model/tsunami_telegram_meta.dart';
+import 'package:eqmonitor/feature/tsunami/data/model/value/tsunami_warning_kind.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'tsunami_timeline.freezed.dart';
@@ -80,84 +90,20 @@ class _TsunamiTimelineMapper {
         RegionTimeline(
           code: r.code,
           name: r.name,
-          kind: [
-            for (final v in r.kind)
-              KindTimelineEntry(
-                kind: v.value,
-                telegramId: v.telegramId,
-                headline: _meta(v.telegramId).headline,
-                title: _meta(v.telegramId).title,
-                publishedAt: _meta(v.telegramId).publishedAt,
-                revokedAt: _meta(v.telegramId).revokedAt,
-              ),
-          ],
-          lastKind: [
-            for (final v in r.lastKind)
-              KindTimelineEntry(
-                kind: v.value,
-                telegramId: v.telegramId,
-                headline: _meta(v.telegramId).headline,
-                title: _meta(v.telegramId).title,
-                publishedAt: _meta(v.telegramId).publishedAt,
-                revokedAt: _meta(v.telegramId).revokedAt,
-              ),
-          ],
+          kind: [for (final v in r.kind) _kindEntry(v)],
+          lastKind: [for (final v in r.lastKind) _kindEntry(v)],
           forecastFirstHeight: [
-            for (final v in r.forecastFirstHeight)
-              FirstHeightTimelineEntry(
-                arrivalTime: v.value?.arrivalTime,
-                condition: v.value?.condition,
-                revise: v.value?.revise,
-                telegramId: v.telegramId,
-                headline: _meta(v.telegramId).headline,
-                title: _meta(v.telegramId).title,
-                publishedAt: _meta(v.telegramId).publishedAt,
-                revokedAt: _meta(v.telegramId).revokedAt,
-              ),
+            for (final v in r.forecastFirstHeight) _forecastFirstHeightEntry(v),
           ],
           forecastMaxHeight: [
-            for (final v in r.forecastMaxHeight)
-              MaxHeightTimelineEntry(
-                value: v.value?.value,
-                isOver: v.value?.isOver,
-                qualitative: v.value?.qualitative,
-                isImportant: v.value?.isImportant,
-                revise: v.value?.revise,
-                telegramId: v.telegramId,
-                headline: _meta(v.telegramId).headline,
-                title: _meta(v.telegramId).title,
-                publishedAt: _meta(v.telegramId).publishedAt,
-                revokedAt: _meta(v.telegramId).revokedAt,
-              ),
+            for (final v in r.forecastMaxHeight) _forecastMaxHeightEntry(v),
           ],
           estimationFirstHeight: [
             for (final v in r.estimationFirstHeight)
-              EstimationFirstHeightTimelineEntry(
-                arrivalTime: v.value?.arrivalTime,
-                isAlreadyArrived: v.value?.isAlreadyArrived,
-                revise: v.value?.revise,
-                telegramId: v.telegramId,
-                headline: _meta(v.telegramId).headline,
-                title: _meta(v.telegramId).title,
-                publishedAt: _meta(v.telegramId).publishedAt,
-                revokedAt: _meta(v.telegramId).revokedAt,
-              ),
+              _estimationFirstHeightEntry(v),
           ],
           estimationMaxHeight: [
-            for (final v in r.estimationMaxHeight)
-              EstimationMaxHeightTimelineEntry(
-                dateTime: v.value?.dateTime,
-                value: v.value?.value,
-                isOver: v.value?.isOver,
-                qualitative: v.value?.qualitative,
-                isObserving: v.value?.isObserving,
-                revise: v.value?.revise,
-                telegramId: v.telegramId,
-                headline: _meta(v.telegramId).headline,
-                title: _meta(v.telegramId).title,
-                publishedAt: _meta(v.telegramId).publishedAt,
-                revokedAt: _meta(v.telegramId).revokedAt,
-              ),
+            for (final v in r.estimationMaxHeight) _estimationMaxHeightEntry(v),
           ],
           stations: [
             for (final s in r.stations)
@@ -165,42 +111,10 @@ class _TsunamiTimelineMapper {
                 code: s.code,
                 name: s.name,
                 forecast: [
-                  for (final v in s.forecast)
-                    StationForecastTimelineEntry(
-                      highTideAt: v.value?.highTideAt,
-                      firstHeightArrivalTime: v.value?.firstHeight?.arrivalTime,
-                      firstHeightCondition: v.value?.firstHeight?.condition,
-                      firstHeightRevise: v.value?.firstHeight?.revise,
-                      telegramId: v.telegramId,
-                      headline: _meta(v.telegramId).headline,
-                      title: _meta(v.telegramId).title,
-                      publishedAt: _meta(v.telegramId).publishedAt,
-                      revokedAt: _meta(v.telegramId).revokedAt,
-                    ),
+                  for (final v in s.forecast) _stationForecastEntry(v),
                 ],
                 observation: [
-                  for (final v in s.observation)
-                    StationObservationTimelineEntry(
-                      sensor: v.value?.sensor,
-                      firstHeightArrivalTime: v.value?.firstHeight.arrivalTime,
-                      firstHeightInitial: v.value?.firstHeight.initial,
-                      firstHeightIsUnidentifiable:
-                          v.value?.firstHeight.isUnidentifiable,
-                      firstHeightIsMissing: v.value?.firstHeight.isMissing,
-                      firstHeightRevise: v.value?.firstHeight.revise,
-                      maxHeightDateTime: v.value?.maxHeight?.dateTime,
-                      maxHeightValue: v.value?.maxHeight?.value,
-                      maxHeightIsOver: v.value?.maxHeight?.isOver,
-                      maxHeightIsRising: v.value?.maxHeight?.isRising,
-                      maxHeightCondition: v.value?.maxHeight?.condition,
-                      maxHeightIsMissing: v.value?.maxHeight?.isMissing,
-                      maxHeightRevise: v.value?.maxHeight?.revise,
-                      telegramId: v.telegramId,
-                      headline: _meta(v.telegramId).headline,
-                      title: _meta(v.telegramId).title,
-                      publishedAt: _meta(v.telegramId).publishedAt,
-                      revokedAt: _meta(v.telegramId).revokedAt,
-                    ),
+                  for (final v in s.observation) _stationObservationEntry(v),
                 ],
               ),
           ],
@@ -212,40 +126,186 @@ class _TsunamiTimelineMapper {
           code: o.code,
           name: o.name,
           firstHeight: [
-            for (final v in o.firstHeight)
-              ObservationFirstHeightTimelineEntry(
-                arrivalTime: v.value.arrivalTime,
-                initial: v.value.initial,
-                isUnidentifiable: v.value.isUnidentifiable,
-                isMissing: v.value.isMissing,
-                revise: v.value.revise,
-                telegramId: v.telegramId,
-                headline: _meta(v.telegramId).headline,
-                title: _meta(v.telegramId).title,
-                publishedAt: _meta(v.telegramId).publishedAt,
-                revokedAt: _meta(v.telegramId).revokedAt,
-              ),
+            for (final v in o.firstHeight) _observationFirstHeightEntry(v),
           ],
           maxHeight: [
-            for (final v in o.maxHeight)
-              ObservationMaxHeightTimelineEntry(
-                dateTime: v.value?.dateTime,
-                value: v.value?.value,
-                isOver: v.value?.isOver,
-                isRising: v.value?.isRising,
-                condition: v.value?.condition,
-                isMissing: v.value?.isMissing,
-                revise: v.value?.revise,
-                telegramId: v.telegramId,
-                headline: _meta(v.telegramId).headline,
-                title: _meta(v.telegramId).title,
-                publishedAt: _meta(v.telegramId).publishedAt,
-                revokedAt: _meta(v.telegramId).revokedAt,
-              ),
+            for (final v in o.maxHeight) _observationMaxHeightEntry(v),
           ],
         ),
     ],
   );
 
-  TsunamiTelegramMeta _meta(String telegramId) => _metaById[telegramId]!;
+  KindTimelineEntry _kindEntry(TrackedValue<TsunamiWarningKind> v) {
+    final meta = _meta(v.telegramId);
+    return KindTimelineEntry(
+      kind: v.value,
+      telegramId: v.telegramId,
+      headline: meta.headline,
+      title: meta.title,
+      publishedAt: meta.publishedAt,
+      revokedAt: meta.revokedAt,
+    );
+  }
+
+  FirstHeightTimelineEntry _forecastFirstHeightEntry(
+    TrackedValue<TsunamiForecastFirstHeight?> v,
+  ) {
+    final meta = _meta(v.telegramId);
+    return FirstHeightTimelineEntry(
+      arrivalTime: v.value?.arrivalTime,
+      condition: v.value?.condition,
+      revise: v.value?.revise,
+      telegramId: v.telegramId,
+      headline: meta.headline,
+      title: meta.title,
+      publishedAt: meta.publishedAt,
+      revokedAt: meta.revokedAt,
+    );
+  }
+
+  MaxHeightTimelineEntry _forecastMaxHeightEntry(
+    TrackedValue<TsunamiForecastMaxHeight?> v,
+  ) {
+    final meta = _meta(v.telegramId);
+    return MaxHeightTimelineEntry(
+      value: v.value?.value,
+      isOver: v.value?.isOver,
+      qualitative: v.value?.qualitative,
+      isImportant: v.value?.isImportant,
+      revise: v.value?.revise,
+      telegramId: v.telegramId,
+      headline: meta.headline,
+      title: meta.title,
+      publishedAt: meta.publishedAt,
+      revokedAt: meta.revokedAt,
+    );
+  }
+
+  EstimationFirstHeightTimelineEntry _estimationFirstHeightEntry(
+    TrackedValue<TsunamiEstimationFirstHeight?> v,
+  ) {
+    final meta = _meta(v.telegramId);
+    return EstimationFirstHeightTimelineEntry(
+      arrivalTime: v.value?.arrivalTime,
+      isAlreadyArrived: v.value?.isAlreadyArrived,
+      revise: v.value?.revise,
+      telegramId: v.telegramId,
+      headline: meta.headline,
+      title: meta.title,
+      publishedAt: meta.publishedAt,
+      revokedAt: meta.revokedAt,
+    );
+  }
+
+  EstimationMaxHeightTimelineEntry _estimationMaxHeightEntry(
+    TrackedValue<TsunamiEstimationMaxHeight?> v,
+  ) {
+    final meta = _meta(v.telegramId);
+    return EstimationMaxHeightTimelineEntry(
+      dateTime: v.value?.dateTime,
+      value: v.value?.value,
+      isOver: v.value?.isOver,
+      qualitative: v.value?.qualitative,
+      isObserving: v.value?.isObserving,
+      revise: v.value?.revise,
+      telegramId: v.telegramId,
+      headline: meta.headline,
+      title: meta.title,
+      publishedAt: meta.publishedAt,
+      revokedAt: meta.revokedAt,
+    );
+  }
+
+  StationForecastTimelineEntry _stationForecastEntry(
+    TrackedValue<TsunamiStationForecast?> v,
+  ) {
+    final meta = _meta(v.telegramId);
+    return StationForecastTimelineEntry(
+      highTideAt: v.value?.highTideAt,
+      firstHeightArrivalTime: v.value?.firstHeight?.arrivalTime,
+      firstHeightCondition: v.value?.firstHeight?.condition,
+      firstHeightRevise: v.value?.firstHeight?.revise,
+      telegramId: v.telegramId,
+      headline: meta.headline,
+      title: meta.title,
+      publishedAt: meta.publishedAt,
+      revokedAt: meta.revokedAt,
+    );
+  }
+
+  StationObservationTimelineEntry _stationObservationEntry(
+    TrackedValue<TsunamiStationObservation?> v,
+  ) {
+    final meta = _meta(v.telegramId);
+    return StationObservationTimelineEntry(
+      sensor: v.value?.sensor,
+      firstHeightArrivalTime: v.value?.firstHeight.arrivalTime,
+      firstHeightInitial: v.value?.firstHeight.initial,
+      firstHeightIsUnidentifiable: v.value?.firstHeight.isUnidentifiable,
+      firstHeightIsMissing: v.value?.firstHeight.isMissing,
+      firstHeightRevise: v.value?.firstHeight.revise,
+      maxHeightDateTime: v.value?.maxHeight?.dateTime,
+      maxHeightValue: v.value?.maxHeight?.value,
+      maxHeightIsOver: v.value?.maxHeight?.isOver,
+      maxHeightIsRising: v.value?.maxHeight?.isRising,
+      maxHeightCondition: v.value?.maxHeight?.condition,
+      maxHeightIsMissing: v.value?.maxHeight?.isMissing,
+      maxHeightRevise: v.value?.maxHeight?.revise,
+      telegramId: v.telegramId,
+      headline: meta.headline,
+      title: meta.title,
+      publishedAt: meta.publishedAt,
+      revokedAt: meta.revokedAt,
+    );
+  }
+
+  ObservationFirstHeightTimelineEntry _observationFirstHeightEntry(
+    TrackedValue<TsunamiObservationFirstHeight> v,
+  ) {
+    final meta = _meta(v.telegramId);
+    return ObservationFirstHeightTimelineEntry(
+      arrivalTime: v.value.arrivalTime,
+      initial: v.value.initial,
+      isUnidentifiable: v.value.isUnidentifiable,
+      isMissing: v.value.isMissing,
+      revise: v.value.revise,
+      telegramId: v.telegramId,
+      headline: meta.headline,
+      title: meta.title,
+      publishedAt: meta.publishedAt,
+      revokedAt: meta.revokedAt,
+    );
+  }
+
+  ObservationMaxHeightTimelineEntry _observationMaxHeightEntry(
+    TrackedValue<TsunamiObservationMaxHeight?> v,
+  ) {
+    final meta = _meta(v.telegramId);
+    return ObservationMaxHeightTimelineEntry(
+      dateTime: v.value?.dateTime,
+      value: v.value?.value,
+      isOver: v.value?.isOver,
+      isRising: v.value?.isRising,
+      condition: v.value?.condition,
+      isMissing: v.value?.isMissing,
+      revise: v.value?.revise,
+      telegramId: v.telegramId,
+      headline: meta.headline,
+      title: meta.title,
+      publishedAt: meta.publishedAt,
+      revokedAt: meta.revokedAt,
+    );
+  }
+
+  TsunamiTelegramMeta _meta(String telegramId) {
+    final meta = _metaById[telegramId];
+    if (meta == null) {
+      throw ArgumentError.value(
+        telegramId,
+        'telegramId',
+        'no telegram meta for id',
+      );
+    }
+    return meta;
+  }
 }
--- app/test/feature/tsunami/tsunami_telegram_timeline_notifier_test.dart
+++ app/test/feature/tsunami/tsunami_telegram_timeline_notifier_test.dart
@@ -2,44 +2,47 @@ import 'package:eqmonitor/feature/tsunami/data/model/timeline/tsunami_timeline.d
 import 'package:eqmonitor/feature/tsunami/data/model/tracking/tracked_region.dart';
 import 'package:eqmonitor/feature/tsunami/data/model/tracking/tracked_tsunami_timeline.dart';
 import 'package:eqmonitor/feature/tsunami/data/model/tracking/tracked_value.dart';
+import 'package:eqmonitor/feature/tsunami/data/model/tsunami_forecast_max_height.dart';
 import 'package:eqmonitor/feature/tsunami/data/model/tsunami_telegram_meta.dart';
+import 'package:eqmonitor/feature/tsunami/data/model/value/qualitative_height.dart';
+import 'package:eqmonitor/feature/tsunami/data/model/value/revise.dart';
 import 'package:eqmonitor/feature/tsunami/data/model/value/tsunami_warning_kind.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('kind の変化点が電文メタと結合されて公開型になる', () {
-    final meta = [
+  // 電文メタを組み立てるヘルパ（テスト内ローカル関数）
+  TsunamiTelegramMeta meta(String id, {required DateTime publishedAt}) =>
       TsunamiTelegramMeta(
-        telegramId: 't1',
+        telegramId: id,
         serialNo: 1,
-        title: 'T1',
+        title: 'title-$id',
         headline: null,
-        publishedAt: DateTime(2026, 1, 15),
-        reportedAt: DateTime(2026, 1, 15),
+        publishedAt: publishedAt,
+        reportedAt: publishedAt,
         targetedAt: null,
         revokedAt: null,
         infoKind: 'k',
-      ),
-    ];
+      );
+
+  test('kind の変化点が電文メタと結合されて公開型になる', () {
     final tracked = TrackedTsunamiTimeline(
-      telegrams: meta,
+      telegrams: [meta('t1', publishedAt: DateTime(2026, 1, 15))],
       regions: [
-        // ignore: prefer_const_constructors
-        TrackedRegion(
+        const TrackedRegion(
           code: '100',
           name: '宮城',
           kind: [
-            const TrackedValue(
+            TrackedValue(
               value: TsunamiWarningKind.warning,
               telegramId: 't1',
             ),
           ],
-          lastKind: const [],
-          forecastFirstHeight: const [],
-          forecastMaxHeight: const [],
-          estimationFirstHeight: const [],
-          estimationMaxHeight: const [],
-          stations: const [],
+          lastKind: [],
+          forecastFirstHeight: [],
+          forecastMaxHeight: [],
+          estimationFirstHeight: [],
+          estimationMaxHeight: [],
+          stations: [],
         ),
       ],
       offshoreStations: const [],
@@ -51,6 +54,51 @@ void main() {
     expect(region.code, '100');
     expect(region.kind.single.kind, TsunamiWarningKind.warning);
     expect(region.kind.single.telegramId, 't1');
-    expect(region.kind.single.title, 'T1');
+    expect(region.kind.single.title, 'title-t1');
+  });
+
+  test('multi-field concern (forecastMaxHeight) が値・メタともに結合される', () {
+    final publishedAt = DateTime(2026, 1, 15, 3);
+    final tracked = TrackedTsunamiTimeline(
+      telegrams: [meta('t9', publishedAt: publishedAt)],
+      regions: [
+        const TrackedRegion(
+          code: '100',
+          name: '宮城',
+          kind: [],
+          lastKind: [],
+          forecastFirstHeight: [],
+          forecastMaxHeight: [
+            TrackedValue(
+              value: TsunamiForecastMaxHeight(
+                value: 5,
+                isOver: true,
+                qualitative: QualitativeHeight.high,
+                isImportant: true,
+                revise: Revise.update,
+              ),
+              telegramId: 't9',
+            ),
+          ],
+          estimationFirstHeight: [],
+          estimationMaxHeight: [],
+          stations: [],
+        ),
+      ],
+      offshoreStations: const [],
+    );
+
+    final entry = tracked.toPublic().regions.single.forecastMaxHeight.single;
+
+    // 追跡項目フィールド
+    expect(entry.value, 5.0);
+    expect(entry.isOver, isTrue);
+    expect(entry.qualitative, QualitativeHeight.high);
+    expect(entry.isImportant, isTrue);
+    expect(entry.revise, Revise.update);
+    // 電文メタ
+    expect(entry.telegramId, 't9');
+    expect(entry.title, 'title-t9');
+    expect(entry.publishedAt, publishedAt);
   });
 }
