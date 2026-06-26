46bca68c5a54b0d8daa9361b6ee69b79d9f09ebc
YumNumm <admin@yumnumm.dev>
2026-06-27 06:21:20 +0900

    fix: eqmonitor_lints_plugin

--- tools/eqmonitor_lints_plugin/lib/main.dart
+++ tools/eqmonitor_lints_plugin/lib/main.dart
@@ -3,12 +3,13 @@ import 'dart:async';
 import 'package:analysis_server_plugin/plugin.dart';
 import 'package:analysis_server_plugin/registry.dart';
 import 'package:analyzer/analysis_rule/analysis_rule.dart';
-import 'package:eqmonitor_lints_plugin/rules/avoid_stateful_widget.dart';
-import 'package:eqmonitor_lints_plugin/rules/avoid_null_assertion_operator.dart';
-import 'package:eqmonitor_lints_plugin/rules/avoid_top_level_functions.dart';
-import 'package:eqmonitor_lints_plugin/rules/avoid_print_call.dart';
-import 'package:eqmonitor_lints_plugin/rules/avoid_eqmonitor_api_in_ui.dart';
-import 'package:eqmonitor_lints_plugin/rules/avoid_mixed_declaration_categories.dart';
+
+import 'rules/avoid_eqmonitor_api_in_ui.dart';
+import 'rules/avoid_mixed_declaration_categories.dart';
+import 'rules/avoid_null_assertion_operator.dart';
+import 'rules/avoid_print_call.dart';
+import 'rules/avoid_stateful_widget.dart';
+import 'rules/avoid_top_level_functions.dart';
 
 final plugin = _EqmonitorLintsPlugin();
 
