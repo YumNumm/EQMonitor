bc5c6e0a9ba6ecffc9cb2ef8e7d8e310f7eff57b
YumNumm <admin@yumnumm.dev>
2026-06-28 03:48:34 +0900

    feat(eew_history): wire EewHistoryPage into router and debug page

--- app/lib/core/router/router.dart
+++ app/lib/core/router/router.dart
@@ -13,6 +13,7 @@ import 'package:eqmonitor/feature/earthquake_history/ui/earthquake_history_page.
 import 'package:eqmonitor/feature/earthquake_search/data/model/earthquake_search_parameter.dart';
 import 'package:eqmonitor/feature/earthquake_search/ui/earthquake_search_result_page.dart';
 import 'package:eqmonitor/feature/eew/ui/page/eew_details_by_event_id_page.dart';
+import 'package:eqmonitor/feature/eew_history/ui/eew_history_page.dart';
 import 'package:eqmonitor/feature/home/ui/page/home_map_layer_page.dart';
 import 'package:eqmonitor/feature/knet_waveform/data/model/knet_station_result.dart';
 import 'package:eqmonitor/feature/knet_waveform/ui/knet_waveform_page.dart';
@@ -160,6 +161,15 @@ class EarthquakeHistoryRoute extends GoRouteData with $EarthquakeHistoryRoute {
   @override
   Widget build(BuildContext context, GoRouterState state) =>
       EarthquakeHistoryPage(initialParameter: $extra);
+}
+
+@TypedGoRoute<EewHistoryRoute>(path: '/eew-history')
+class EewHistoryRoute extends GoRouteData with $EewHistoryRoute {
+  const EewHistoryRoute();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) =>
+      const EewHistoryPage();
 }
 
 @TypedGoRoute<EarthquakeSearchResultRoute>(
