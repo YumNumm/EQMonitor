bb43e861f8cd38653a06c5d7fdbb5beb1fbdf6d1
Ryotaro Onoue <r_onoue@yumemi.co.jp>
2026-07-01 02:28:40 +0900

    fix: 震央誤差の表示バグを修正
    Signed-off-by: Ryotaro Onoue <r_onoue@yumemi.co.jp>

--- app/lib/feature/earthquake_history/data/model/earthquake.dart
+++ app/lib/feature/earthquake_history/data/model/earthquake.dart
@@ -2,6 +2,7 @@ import 'package:eqmonitor/core/model/telegram/telegram_status.dart';
 import 'package:eqmonitor/feature/earthquake_history/data/model/earthquake_data_source.dart';
 import 'package:eqmonitor/feature/earthquake_history/data/model/earthquake_hypocenter.dart';
 import 'package:eqmonitor/feature/earthquake_history/data/model/earthquake_intensity.dart';
+import 'package:eqmonitor/feature/earthquake_history/data/model/earthquake_telegram_type.dart';
 import 'package:eqmonitor/feature/earthquake_history/data/model/origin_time_precision.dart';
 import 'package:eqmonitor/feature/parameter/data/model/parameter.dart';
 import 'package:eqmonitor_api/eqmonitor_api.dart' as api;
@@ -19,6 +20,7 @@ abstract class Earthquake with _$Earthquake {
     required OriginTimePrecision originTimePrecision,
     required DateTime? arrivalTime,
     required EarthquakeDataSource dataSource,
+    required List<EarthquakeTelegramType> telegramTypes,
     required EarthquakeHypocenter? hypocenter,
     required EarthquakeIntensity? intensity,
 
@@ -40,6 +42,10 @@ extension EarthquakeApiExtension on api.Earthquake {
     originTimePrecision: originTimePrecision.toOriginTimePrecision,
     arrivalTime: arrivalTime,
     dataSource: datasource.toEarthquakeDataSource,
+    telegramTypes: telegrams
+        .map((e) => e.telegram.type.toEarthquakeTelegramTypeOrNull)
+        .whereType<EarthquakeTelegramType>()
+        .toList(),
     hypocenter: hypocenter?.toEarthquakeHypocenter,
     estimatedIntensityTileUrl: estimatedIntensityTile,
     intensity: intensity?.toEarthquakeIntensity(
--- app/lib/feature/earthquake_history/ui/layer/earthquake_history_hypocenter_error_layer.dart
+++ app/lib/feature/earthquake_history/ui/layer/earthquake_history_hypocenter_error_layer.dart
@@ -7,6 +7,7 @@ import 'package:eqmonitor/core/util/map/hypocenter_error_range_util.dart';
 import 'package:eqmonitor/feature/earthquake_history/data/model/coordinate.dart';
 import 'package:eqmonitor/feature/earthquake_history/data/model/earthquake.dart';
 import 'package:eqmonitor/feature/earthquake_history/data/model/earthquake_history_map_layer_parameter.dart';
+import 'package:eqmonitor/feature/earthquake_history/data/model/earthquake_telegram_type.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -48,9 +49,16 @@ class EarthquakeHistoryHypocenterErrorLayer extends HookConsumerWidget {
                 return;
               }
 
+              final decimalPlaces =
+                  earthquake.telegramTypes.contains(
+                    EarthquakeTelegramType.vxse61,
+                  )
+                  ? 3
+                  : 1;
               final polygon = hypocenterErrorPolygon(
-                coords.latitude,
-                coords.longitude,
+                lat: coords.latitude,
+                lon: coords.longitude,
+                decimalPlaces: decimalPlaces,
               );
 
               await styleController.addSource(
