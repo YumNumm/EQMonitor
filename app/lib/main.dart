f3ff84ac296f295e09f05ae256a5bda9d4219240
Ryotaro Onoue <r_onoue@yumemi.co.jp>
2026-07-03 04:50:54 +0900

    feat(error): 致命的エラー画面(GoRouter/ErrorWidget)を整備

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -9,6 +9,7 @@ import 'package:core/core.dart' as core;
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:eqmonitor/app.dart';
 import 'package:eqmonitor/core/component/error/error_card.dart';
+import 'package:eqmonitor/core/component/error/fatal_error_screen.dart';
 import 'package:eqmonitor/core/data/preferences/shared/shared_preferences.dart'
     as data_prefs;
 import 'package:eqmonitor/core/fcm/channels.dart';
@@ -137,6 +138,9 @@ Future<void> _main() async {
       'Uncaught fatal exception',
     );
   };
+  if (!kDebugMode) {
+    ErrorWidget.builder = buildFatalErrorWidget;
+  }
   PlatformDispatcher.instance.onError = (exception, stackTrace) {
     talker.handle(
       exception,
