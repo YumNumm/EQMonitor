627144ea1f146b736b94fd6491efab50adcad3ad
YumNumm <admin@yumnumm.dev>
2026-07-18 16:50:06 +0900

    Fix seismicityのGeoJSON取得にHTTPキャッシュを追加

--- app/lib/feature/seismicity/data/provider/seismicity_repository_provider.dart
+++ app/lib/feature/seismicity/data/provider/seismicity_repository_provider.dart
@@ -1,4 +1,6 @@
+import 'package:cache/cache.dart';
 import 'package:dio/dio.dart';
+import 'package:eqmonitor/core/api/http_cache_store_provider.dart';
 import 'package:eqmonitor/core/provider/dio_base_options.dart';
 import 'package:eqmonitor/core/provider/dio_provider.dart';
 import 'package:eqmonitor/feature/seismicity/data/repository/seismicity_repository.dart';
@@ -7,16 +9,18 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'seismicity_repository_provider.g.dart';
 
 @Riverpod(keepAlive: true)
-Dio seismicityGeoJsonDio(Ref ref) {
+Future<Dio> seismicityGeoJsonDio(Ref ref) async {
+  final httpCache = await ref.watch(httpCacheStoreProvider.future);
   final dio = Dio(buildApiBaseOptions(baseUrl: ''));
   dio.options.connectTimeout = const Duration(milliseconds: 10000);
   dio.options.sendTimeout = const Duration(milliseconds: 10000);
+  dio.interceptors.add(HttpCacheInterceptor(httpCache));
   return dio;
 }
 
 @Riverpod(keepAlive: true)
 Future<SeismicityRepository> seismicityRepository(Ref ref) async {
   final manifestDio = await ref.watch(dioProvider.future);
-  final geoJsonDio = ref.watch(seismicityGeoJsonDioProvider);
+  final geoJsonDio = await ref.watch(seismicityGeoJsonDioProvider.future);
   return SeismicityRepository(manifestDio: manifestDio, geoJsonDio: geoJsonDio);
 }
