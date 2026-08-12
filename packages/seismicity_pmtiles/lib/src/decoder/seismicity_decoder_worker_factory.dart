ae09d8ead666edf8739560ea4e7bad015af6b2c1
Ryotaro Onoue <r_onoue@yumemi.co.jp>
2026-08-13 05:58:32 +0900

    Feat: decoder worker interfaceを追加

---
+++ packages/seismicity_pmtiles/lib/src/decoder/seismicity_decoder_worker_factory.dart
@@ -0,0 +1,21 @@
+import 'dart:isolate';
+import 'package:seismicity_pmtiles/src/model/seismicity_pmtiles_archive_descriptor.dart';
+import 'package:seismicity_pmtiles/src/model/seismicity_pmtiles_dataset.dart';
+import 'package:seismicity_pmtiles/src/model/seismicity_pmtiles_decode_progress.dart';
+
+abstract interface class SeismicityDecoderWorkerFactory {
+  Future<SeismicityDecoderWorkerHandle> spawn({
+    required SeismicityPmTilesArchiveDescriptor acceptedDescriptor,
+    required int chunkCapacity,
+  });
+}
+
+abstract interface class SeismicityDecoderWorkerHandle {
+  Future<SeismicityPmTilesDecodeProgress> decode({
+    required TransferableTypedData tileBytes,
+  });
+  Future<SeismicityPmTilesDataset> finish();
+  Future<void> cancel();
+  Future<void> close();
+  Future<void> get retired;
+}
