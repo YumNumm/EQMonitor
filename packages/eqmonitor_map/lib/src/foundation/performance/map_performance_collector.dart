55aa1db52fb91009382cad2617f45d2a66be4207
Ryotaro Onoue <r_onoue@yumemi.co.jp>
2026-08-13 02:39:48 +0900

    Feat: 性能sampleのversionとclockを検証

--- packages/eqmonitor_map/lib/src/foundation/performance/map_performance_collector.dart
+++ packages/eqmonitor_map/lib/src/foundation/performance/map_performance_collector.dart
@@ -1,3 +1,6 @@
+import 'package:eqmonitor_map/src/foundation/performance/map_metric_aggregate.dart';
+import 'package:eqmonitor_map/src/foundation/performance/map_performance_event.dart';
+import 'package:eqmonitor_map/src/foundation/performance/map_performance_metric.dart';
 import 'package:eqmonitor_map/src/foundation/performance/map_performance_policy.dart';
 
 enum MapPerformanceRecordResultKind {
@@ -40,10 +43,12 @@ final class MapPerformanceCollector {
   final MapPerformancePolicy policy;
   final Duration windowStartedAt;
 
-  final _acceptedCount = 0;
-  final _aggregatedCount = 0;
-  final _ignoredCount = 0;
-  final _rejectedCount = 0;
+  final Map<MapPerformanceMetricKind, MapMetricAccumulator> _aggregates = {};
+  Duration? _lastMonotonicAt;
+  var _acceptedCount = 0;
+  var _aggregatedCount = 0;
+  var _ignoredCount = 0;
+  var _rejectedCount = 0;
   final _rateLimitedCount = 0;
   final _droppedCount = 0;
   final _bufferedEventCount = 0;
@@ -61,4 +66,31 @@ final class MapPerformanceCollector {
   int get droppedCount => _droppedCount;
 
   int get bufferedEventCount => _bufferedEventCount;
+
+  MapPerformanceRecordResult record(MapPerformanceEvent event) {
+    final lastMonotonicAt = _lastMonotonicAt;
+    if (event.schemaVersion != policy.schemaVersion ||
+        event.clockDomain != policy.clockDomain ||
+        lastMonotonicAt != null && event.sample.monotonicAt < lastMonotonicAt) {
+      _rejectedCount += 1;
+      return const MapPerformanceRecordResult.rejected();
+    }
+    _lastMonotonicAt = event.sample.monotonicAt;
+
+    if (policy.observationLevel == MapPerformanceObservationLevel.off) {
+      _ignoredCount += 1;
+      return const MapPerformanceRecordResult.ignored();
+    }
+
+    (_aggregates[event.sample.kind] ??= MapMetricAccumulator(
+      policy.reservoirCapacity,
+    )).add(event.sample.value);
+    _aggregatedCount += 1;
+    if (policy.observationLevel == MapPerformanceObservationLevel.aggregate) {
+      return const MapPerformanceRecordResult.aggregated();
+    }
+
+    _acceptedCount += 1;
+    return const MapPerformanceRecordResult.accepted();
+  }
 }
