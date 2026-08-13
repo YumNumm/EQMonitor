4414801c68da47d8126f98df9f82bf6ee20f9a6c
Ryotaro Onoue <r_onoue@yumemi.co.jp>
2026-08-14 02:35:35 +0900

    Perf: benchmark worker数を計測

---
+++ packages/seismicity_pmtiles/benchmark/support/counting_decoder_worker_factory.dart
@@ -0,0 +1,28 @@
+import 'package:seismicity_pmtiles/src/decoder/seismicity_decoder_worker_factory.dart';
+import 'package:seismicity_pmtiles/src/model/seismicity_pmtiles_archive_descriptor.dart';
+
+/// Benchmark-only spawn counter that delegates unchanged to [delegate].
+///
+/// Increments [spawnCount] once immediately before each [delegate.spawn] call,
+/// including attempts that later fail.
+final class CountingDecoderWorkerFactory
+    implements SeismicityDecoderWorkerFactory {
+  CountingDecoderWorkerFactory({required this.delegate});
+
+  final SeismicityDecoderWorkerFactory delegate;
+
+  var _spawnCount = 0;
+  int get spawnCount => _spawnCount;
+
+  @override
+  Future<SeismicityDecoderWorkerHandle> spawn({
+    required SeismicityPmTilesArchiveDescriptor acceptedDescriptor,
+    required int chunkCapacity,
+  }) {
+    _spawnCount += 1;
+    return delegate.spawn(
+      acceptedDescriptor: acceptedDescriptor,
+      chunkCapacity: chunkCapacity,
+    );
+  }
+}
