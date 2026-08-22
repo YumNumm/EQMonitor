daa7fd1ff26d418d03f60b57fb9603b4106a24e1
YumNumm <admin@yumnumm.dev>
2026-08-22 18:21:02 +0900

    chore: 不要なimportの削除

--- app/lib/feature/kyoshin_monitor/page/components/connection_status_card.dart
+++ app/lib/feature/kyoshin_monitor/page/components/connection_status_card.dart
@@ -1,6 +1,5 @@
 import 'package:eqmonitor/core/designsystem/design_system_build_context_x.dart';
 import 'package:eqmonitor/core/provider/connectivity/connectivity_provider.dart';
-import 'package:eqmonitor/core/realtime/data_source/eqmonitor/eqmonitor_ws_ping_probe.dart';
 import 'package:eqmonitor/core/realtime/data_source/eqmonitor/eqmonitor_ws_status_notifier.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:material_ui/material_ui.dart';
