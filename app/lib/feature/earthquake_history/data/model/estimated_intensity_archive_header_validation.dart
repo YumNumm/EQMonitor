ba011c95c9596ef53f350d49af7426a9e25b302b
YumNumm <admin@yumnumm.dev>
2026-08-26 02:17:57 +0900

    feat: 推計震度header制約を検証

---
+++ app/lib/feature/earthquake_history/data/model/estimated_intensity_archive_header_validation.dart
@@ -0,0 +1,9 @@
+enum EstimatedIntensityArchiveHeaderFailure {
+  invalidArchive,
+  invalidTileType,
+  invalidTileCompression,
+  invalidZoomRange,
+  invalidBounds,
+  storageFailure,
+  closeFailure,
+}
