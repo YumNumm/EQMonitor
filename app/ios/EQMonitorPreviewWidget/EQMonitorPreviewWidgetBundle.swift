4212482c188d7453a747083a6fc76b758393a5ba
YumNumm <admin@yumnumm.dev>
2026-09-21 01:32:01 +0900

    fix: プレビュー用Bundleをライブアクティビティに限定

--- app/ios/EQMonitorPreviewWidget/EQMonitorPreviewWidgetBundle.swift
+++ app/ios/EQMonitorPreviewWidget/EQMonitorPreviewWidgetBundle.swift
@@ -10,7 +10,6 @@ import WidgetKit
 @main
 struct EQMonitorPreviewWidgetBundle: WidgetBundle {
     var body: some Widget {
-        EarthquakeWidget()
         if #available(iOS 16.1, *) {
             EewLiveActivityWidget()
             EarthquakeLiveActivityWidget()
