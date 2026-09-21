94b72c86a50e0200c4002b2c63f7e3e3e101a8be
Ryotaro Onoue <admin@yumnumm.dev>
2026-09-21 14:54:32 +0000

    refactor: chipの基本部品をM3Eへ移行 (4/78)

--- app/lib/core/component/chip/status_filter_chip.dart
+++ app/lib/core/component/chip/status_filter_chip.dart
@@ -1,7 +1,8 @@
 import 'package:eqmonitor/core/designsystem/design_system_build_context_x.dart';
 import 'package:eqmonitor/core/model/telegram/telegram_status.dart';
-import 'package:material_ui/material_ui.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:m3e_core/m3e_core.dart';
+import 'package:material_ui/material_ui.dart';
 
 class StatusFilterChip extends StatelessWidget {
   const new({required this.statuses, this.onChanged, super.key});
@@ -118,11 +119,11 @@ class _StatusFilterModal extends HookWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                TextButton(
+                M3ETextButton(
                   onPressed: () => Navigator.of(context).pop(),
                   child: const Text('キャンセル'),
                 ),
-                TextButton(
+                M3ETextButton(
                   onPressed: () =>
                       Navigator.of(context)
                           .pop(selectedStatuses.value.toList()),
--- app/lib/core/component/chip/telegram_type_filter_chip.dart
+++ app/lib/core/component/chip/telegram_type_filter_chip.dart
@@ -1,7 +1,8 @@
 import 'package:eqmonitor/core/designsystem/design_system_build_context_x.dart';
 import 'package:eqmonitor/feature/earthquake_history/data/model/earthquake_telegram_type.dart';
-import 'package:material_ui/material_ui.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:m3e_core/m3e_core.dart';
+import 'package:material_ui/material_ui.dart';
 
 class TelegramTypeFilterChip extends StatelessWidget {
   const new({this.telegramTypes, this.onChanged, super.key});
@@ -126,11 +127,11 @@ class _TelegramTypeFilterModal extends HookWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                TextButton(
+                M3ETextButton(
                   onPressed: () => Navigator.of(context).pop(),
                   child: const Text('キャンセル'),
                 ),
-                TextButton(
+                M3ETextButton(
                   onPressed: () => Navigator.of(context).pop(
                     selected.value.toList()
                       ..sort((a, b) => a.index.compareTo(b.index)),
